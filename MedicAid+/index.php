61d6d500ee9d310aacc09a75d1f4323691855476
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-03-29 16:45:17 +0600

    Update index.php

--- MedicAid+/index.php
+++ MedicAid+/index.php
@@ -13,7 +13,7 @@
   </head>
   <body>
     <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+      <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
     <a class="navbar-brand" href="#">
       <img src="Image/Main Logo.jpg" alt="Logo" style="width:150px"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,16 +24,16 @@
 
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" style="font-size:24px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Volunteer</a>
+          <a class="nav-link" style="font-size:24px; font-weight:bold" href="#">Volunteer</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
+          <a class="nav-link" style="font-size:24px; font-weight:bold" href="#">About Us</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" style="font-size:24px; font-weight:bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Dropdown
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
