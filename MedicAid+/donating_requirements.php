f44c8d8d813d1ef7d76a18a057f6832c4219496d
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-03-31 02:01:11 +0600

    Create donating_requirements.php

---
+++ MedicAid+/donating_requirements.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a class="navbar-brand" href="#">
+      <img src="Image/Main Logo.jpg" alt="Logo" style="width:153px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">About Us</a>
+        </li>
+        <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" style="font-size:23px; font-weight:bold" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+    </nav>
+    </header>
+<!-- Background Section-->
+<div class="container">
+  <div class="container-fluid">
+    <h1 class="text-center text-dark text-capitalize pt-5">Donating Criteria</h1>
+    <hr class="w-25 pt-5">
+  </div>
+  <div class="btn-group-vertical w-100">
+    <div class="btn-group">
+      <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+      Age
+      </button>
+      <div class="dropdown-menu w-100">
+        <span class="dropdown-item-text">You must be at least 18 years old to donate to the general blood supply. There is no upper age limit for blood donation as long as you are well with no restrictions or limitations to your activities.</span>
+      </div>
+    </div>
+      <div class="btn-group">
+      <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+        Allergy
+      </button>
+      <div class="dropdown-menu w-100">
+        <span class="dropdown-item-text">Acceptable as long as you feel well, have no fever, and have no problems breathing through your mouth.</span>
+      </div>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+        Antibiotics
+      </button>
+      <div class="dropdown-menu w-100">
+        <span class="dropdown-item-text">A donor with an acute infection should not donate. The reason for antibiotic use must be evaluated to determine if the donor has a bacterial infection that could be transmissible by blood. Acceptable after finishing oral antibiotics for an infection (bacterial or viral). May have taken last pill on the date of donation. Antibiotic by injection for an infection acceptable 10 days after last injection. Acceptable if you are taking antibiotics to prevent an infection for the following reasons: acne, chronic prostatitis, peptic ulcer disease, periodontal disease, pre-dental work, rosacea, ulcerative colitis, after a splenectomy or valvular heart disease. If you have a temperature above 99.5 F, you may not donate.</span>
+      </div>
+    </div>
+    <div class="btn-group">
+    <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+      Aspirin
+    </button>
+    <div class="dropdown-menu w-100">
+      <span class="dropdown-item-text">Aspirin, no waiting period for donating whole blood. However, you must wait 2 full days after taking aspirin or any medication containing aspirin before donating platelets by apheresis.</span>
+    </div>
+  </div>
+  <div class="btn-group">
+    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+      Asthma
+    </button>
+    <div class="dropdown-menu w-100">
+      <span class="dropdown-item-text">Acceptable as long as you do not have any limitations on daily activities and are not having difficulty breathing at the time of donation and you otherwise feel well. Medications for asthma do not disqualify you from donating.</span>
+    </div>
+  </div>
+  <div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+    Birth Control
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text">Women on oral contraceptives or using other forms of birth control are eligible to donate.</span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+    Blood Pressure-High
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text">Acceptable as long as your blood pressure is below 180 systolic (first number) and below 100 diastolic (second number) at the time of donation. Medications for high blood pressure do not disqualify you from donating.</span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+    Blood Pressure-Low
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text">Acceptable as long as you feel well when you come to donate, and your blood pressure is at least 90/50 (systolic/diastolic).</span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+    Blood Transfusion
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text">Wait for 3 months after receiving a blood transfusion from another person in the United States.</span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+    Cancer
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text">Eligibility depends on the type of cancer and treatment history. If you had leukemia or lymphoma, including Hodgkin’s Disease and other cancers of the blood, you are not eligible to donate. Other types of cancer are acceptable if the cancer has been treated successfully and it has been more than 12 months since treatment was completed and there has been no cancer recurrence in this time.</span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+    Cold, Flu
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text">Wait if you have a fever or a productive cough (bringing up phlegm). Wait if you do not feel well on the day of donation. Wait until you have completed antibiotic treatment for sinus, throat or lung infection.</span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+    Cancer
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text"></span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+    Blood Transfusion
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text"></span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+    Cancer
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text"></span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+    Blood Transfusion
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text"></span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+    Cancer
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text"></span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+    Blood Transfusion
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text"></span>
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+    Cancer
+  </button>
+  <div class="dropdown-menu w-100">
+    <span class="dropdown-item-text"></span>
+  </div>
+</div>
+
+
+
+    </div>
+  </div>
+</div>
+
+  </body>
+</html>
