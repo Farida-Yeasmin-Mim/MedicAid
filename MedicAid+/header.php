83f0be4f27507a7ef07b24b47d705e12420f0573
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-03-30 15:51:29 +0600

    Update header.php

--- MedicAid+/header.php
+++ MedicAid+/header.php
@@ -12,9 +12,9 @@
   <link rel="stylesheet" href="CSS/header.css">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   <a class="navbar-brand" href="#">
-    <img src="Image/Main Logo.jpg" alt="Logo" style="width:150px"></a>
+    <img src="Image/Main Logo.jpg" alt="Logo" style="width:153px"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
