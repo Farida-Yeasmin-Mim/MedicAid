ea366841ede54cb1e33ea2681cab91efa0be5d2b
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-03-30 13:56:17 +0600

    Update contact.php

--- MedicAid+/contact.php
+++ MedicAid+/contact.php
@@ -1,47 +1,28 @@
-<div class="container">
-			<div class="row">
-				<div class="col-sm text-center">
-				  <h1 class="div-heading display-4">Contact US</h1>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<form>
-
-					  <div class="form-group">
-						  <input type="name" class="form-control" id="exampleInputName" placeholder="Full Name...">
-					  </div>
-
-            <div class="form-group">
-						  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address...">
-					  </div>
-
-					  <div class="form-group">
-              <select class="form-control" id="exampleFormControlSelect1" placeholder="Select Country...">
-                <option>Bangladesh</option>
-                <option>America</option>
-                <option>China</option>
-                <option>Singapore</option>
-              </select>
-					  </div>
-
-            <div class="form-group">
-              <textarea class="form-control" aria-label="With textarea"></textarea>
-            </div>
-					  <button type="submit" class="btn btn-warning btn-lg btn-block">Submit</button>
-					</form>
-			  </div>
-
-			  <div  class="col-md-6">
-          <h5>Address: <small class="text-muted">Venkatadri IT Park, HP Avenue, Konnappana, Electronic city, Bengaluru, Karnataka 560069</small></h5>
-          <h5>Email: <small class="text-muted">hire@luxurytaxicab.com</small></h5>
-          <h5>Contact: <small class="text-muted">+91 98765 10278 || +91 76589 14244</small></h5>
-          <!-- <p><iframe width="600" height="450" frameborder="0" style="border:0"
-src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJtcNatvRsrjsRA38LEBPt_78&key=..." allowfullscreen></iframe></p> -->
-          <div class="text-center">
-            <img class="img-fluid contact-image" alt="Responsive image" src="https://csds.qld.edu.au/sdc/resources/images/find-us-map.jpg" class="rounded" alt="...">
-          </div>
-	  		</div>
-
-			</div>
-		</div>
+<div class="container-fluid bg-grey">
+  <h2 class="text-center">CONTACT</h2>
+  <div class="row">
+    
+    <div class="col-sm-5">
+      <p>Contact us and we'll get back to you within 24 hours.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Chicago, US</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +00 1515151515</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> myemail@something.com</p>
+    </div>
+    <div class="col-sm-7">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button class="btn btn-default pull-right" type="submit">Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
