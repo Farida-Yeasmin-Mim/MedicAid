fe0f41c7e4786656691d8885cbe93f1b7abe6346
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-04-01 10:46:27 +0600

    User login created

--- MedicAid+/user_login.php
+++ MedicAid+/user_login.php
@@ -3,8 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title>User Log In</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   </head>
   <body>
-<h1>hello</h1>
+    <div class="container-fluid ">
+      <h1 class="text-center text-dark text-capitalize pt-5">Login</h1>
+      <hr class="w-25 pt-4">
+      <div class="w-25 mx-auto">
+        <form action="/action_page.php">
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" placeholder="Enter email" id="email" autocomplete="off">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+  </div>
+  <div class="col-md-12 text-center">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</div>
+</form>
+
+
+      </div>
+    </div>
+
   </body>
 </html>
