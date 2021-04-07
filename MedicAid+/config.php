fc0492ee63d35c364b3b9bbf651a0a764e1aa809
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-04-07 21:11:41 +0600

    Update config.php

--- MedicAid+/config.php
+++ MedicAid+/config.php
@@ -5,6 +5,7 @@
 	$gClient->setClientId("917936359040-oi7v9cvl7ncq77bdvlc1816etq01tvv4.apps.googleusercontent.com");
 	$gClient->setClientSecret("AVrcGI0kvjDSlFsb_gFI5Yuf");
 	$gClient->setApplicationName("MAP Login");
+	
 	$gClient->setRedirectUri("http://localhost/Project/CSE327_Project/MedicAid+/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
