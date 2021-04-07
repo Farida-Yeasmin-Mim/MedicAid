cc896914ace11e2d21c7a73111c53ced3fb2a0cc
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-04-07 21:28:46 +0600

    Update contactus.php

--- MedicAid+/contactus.php
+++ MedicAid+/contactus.php
@@ -2,16 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Contact us</title>
-    <meta charset="utf-8">
+    <title>Contact Us</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="CSS/header.css">
+    <!-- contactus.css-->
+    <link rel="stylesheet" href="CSS/contactus.css">
   </head>
   <body>
+
+    <header>
+      <?php include 'header.php';?>
+    </header>
 
     <div class="Developed__By">
     <div class="container">
@@ -25,11 +25,10 @@
         <div class="team-members">
 
           <div class="member">
-            <img src="Images/member1.jpg">
-
+            <img src="Image/member1.jpg">
             <div class="inner">
               <div class="info">
-                <h5>Farida Yeasmin Mim</h5>
+                <h5>Kazi Ramisa Rifa</h5>
                 <p>Designer</p>
                 <div class="social-links">
                   <a href="https://www.facebook.com/yeasminmim"><span class="fa fa-facebook"></span></a>
@@ -39,15 +38,13 @@
                 </div>
               </div>
             </div>
-
           </div>
 
           <div class="member">
-            <img src="Images/member2.jpg">
-
+            <img src="Image/member2.jpg">
             <div class="inner">
               <div class="info">
-                <h5>Kazi Ramisa Rifa</h5>
+                <h5>Farida Yeasmin Mim</h5>
                 <p>Designer</p>
                 <div class="social-links">
                   <a href="https://www.facebook.com/ramisa.rifa01/"><span class="fa fa-facebook"></span></a>
@@ -57,15 +54,13 @@
                 </div>
               </div>
             </div>
-
           </div>
 
           <div class="member">
-            <img src="Images/Abid.jpg">
-
+            <img src="Image/member03.jpg">
             <div class="inner">
               <div class="info">
-                <h5>Abid Hossain Fahim</h5>
+                <h5>Zarin Tasnim</h5>
                 <p>Designer</p>
                 <div class="social-links">
                   <a href="https://www.facebook.com/abidfahim.bd"><span class="fa fa-facebook"></span></a>
@@ -75,7 +70,6 @@
                 </div>
               </div>
             </div>
-
           </div>
 
         </div>
@@ -84,7 +78,6 @@
 
     </div>
     </div>
-
-    
+    <footer><?php include 'footer.php';?></footer>
   </body>
 </html>
