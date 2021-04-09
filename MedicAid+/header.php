d8ff57d610d8ab17280e81e8d35f5a4383eb3da6
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-04-09 15:00:11 +0600

    Update header.php

--- MedicAid+/header.php
+++ MedicAid+/header.php
@@ -23,17 +23,17 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
     <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Donation</a>
+      </li>
+      <li class="nav-item active">
         <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">More</a>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Ambulance</a>
       </li>
     </ul>
 
