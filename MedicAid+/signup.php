0a425d7ea02acf86d7174d8a44a9e5ee58d234b6
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-04-11 01:22:00 +0600

    Update signup.php

--- MedicAid+/signup.php
+++ MedicAid+/signup.php
@@ -6,27 +6,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="CSS/signup.css">
+   
+
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+    
    </head>
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+   <link rel="stylesheet" href="CSS/signup.css">
+   <body>
 
-
-
-
-
-
-
-
+   	<div class="container">
 <div class="card bg-light">
 <article class="card-body mx-auto" style="max-width: 400px;">
-	<h4 class="card-title mt-3 text-center">SIGN-UP</h4>
+	<h4 class="card-title mt-3 text-center">Sign-Up</h4>
 	<p class="text-center">Get started with your free account</p>
 	<p>
-		<a href="" class="btn btn-block btn-twitter"> <i class="fab fa-twitter"></i>   Login via Twitter</a>
-		<a href="" class="btn btn-block btn-facebook"> <i class="fab fa-facebook-f"></i>   Login via facebook</a>
+
+	<a class="btn btn-lg btn-google btn-block btn-outline " href="#"><img src="https://img.icons8.com/color/16/000000/google-logo.png"> Sign in with Google</a>
+
 	</p>
 	<p class="divider-text">
         <span class="bg-light">OR</span>
@@ -36,29 +35,29 @@
 		<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
 		 </div>
-        <input name="" class="form-control" placeholder="Full name" type="text">
-    </div> <!-- form-group// -->
+        <input name="" class="form-control" placeholder="Full name" type="text" required="">
+    </div> 
     <div class="form-group input-group">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
 		 </div>
-        <input name="" class="form-control" placeholder="Email address" type="email">
-    </div> <!-- form-group// -->
+        <input name="" class="form-control" placeholder="Email address" type="email" required="">
+    </div> 
     <div class="form-group input-group">
     	<div class="input-group-prepend">
-		    <span class="input-group-text"> <i class="fa fa-phone"></i> </span>
+		    <span class="input-group-text"> <i class="fa fa-phone">  </i> </span>
 		</div>
-		<select class="custom-select" style="max-width: 120px;">
+		<select class="custom-select" style="max-width: 120px;" required="" >
 		    <option selected="">+880</option>
 		    
 		</select>
-    	<input name="" class="form-control" placeholder="Phone number" type="text">
-    </div> <!-- form-group// -->
+    	<input name="" class="form-control" placeholder="Phone number" type="text"  required="">
+    </div> 
     <div class="form-group input-group">
     	<div class="input-group-prepend">
-		    <span class="input-group-text"> <i class="fa fa-building"></i> </span>
+		    <span class="input-group-text"    >  <i class="fa fa-medkit"></i> </span> 
 		</div>
-		<select class="form-control">
+		<select class="form-control" required="">
 			<option selected=""> Select Blood Group</option>
 			<option> A+</option>
 			<option> A-</option>
@@ -68,20 +67,21 @@
 			<option> AB-</option>
 			<option> O+</option>
 			<option> O-</option>
+
 		</select>
 	</div> 
     <div class="form-group input-group">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
 		</div>
-        <input class="form-control" placeholder="Create password" type="password">
+        <input class="form-control" placeholder="Create password" type="password" required=" ">
     </div> 
     <div class="form-group input-group">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
 		</div>
         <input class="form-control" placeholder="Repeat password" type="password">
-    </div>                                       
+    </div>                                      
     <div class="form-group">
         <button type="submit" class="btn btn-primary btn-block"> Create Account  </button>
     </div>      
@@ -96,13 +96,18 @@
 <br><br>
 <article class="bg-secondary mb-3">  
 <div class="card-body text-center">
-    <h3 class="text-white mt-3">MedicAid+</h3>
-<p class="h5 text-white">Health Comes First </p>   <br>
+    <img src="Image/Main Logo.jpg" alt="Logo" style="width:149px"></a>
+<p class="h5 text-white">"Health Comes First" </p> 
+
+    
+  <br>
 
 
 </div>
 <br><br>
 </article>
 </nav>
-</header>
+</body>
+</html>
+
 
