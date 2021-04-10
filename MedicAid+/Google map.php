f46f68b162c6b613920d60177b53f5399da8a61a
zarintasnimtur <tasnimturnawork@gmail.com>
2021-04-11 02:07:43 +0600

    update map
    Updated google map

---
+++ MedicAid+/Google map.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	
+</head>
+<body>
+ <section>
+ 	
+ 	   <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29186.928981510697!2d90.36077837136212!3d23.87663260973039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1618084328576!5m2!1sen!2sbd" width="100%" height="1005" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+ </section>
+</body>
+</html>
+
