31213296ddf1602b131a84477cba8d5e5b1ea0c8
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-04-14 05:31:41 +0600

    Modification, Creation, Database Connection
    Modification of signup , creating blood donor sign up page and connected this to database

--- MedicAid+/user_signup.php
+++ MedicAid+/user_signup.php
@@ -1,3 +1,46 @@
+<?php
+include 'connection.php';
+
+if(isset($_POST['signup'])){
+
+$dbfname = mysqli_real_escape_string($conn, $_POST['fullname']);
+$dbemail = mysqli_real_escape_string($conn, $_POST['email']);
+$dbcontact = mysqli_real_escape_string($conn, $_POST['contact']);
+$dbgender = mysqli_real_escape_string($conn, $_POST['gender']);
+$dbage = mysqli_real_escape_string($conn, $_POST['age']);
+$dbbgroup = mysqli_real_escape_string($conn, $_POST['bgroup']);
+$dbdivision = mysqli_real_escape_string($conn, $_POST['division']);
+$dbaddress = mysqli_real_escape_string($conn, $_POST['address']);
+$dbpassword = mysqli_real_escape_string($conn, $_POST['password']);
+
+//INSERT INTO `user`( `User_Name`, `First_Name`, `Last_Name`, `Date_of_birth`, `Country`, `Contact_Number`, `Email`, `Password`, `Premium_ID`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
+$sql = "SELECT email FROM blood_donor";
+$result = mysqli_query($conn,$sql);
+
+if (mysqli_num_rows($result) > 0) {
+// output data of each row
+while($row = mysqli_fetch_assoc($result)) {
+$dbbemail= $row["email"];
+// Check if the username they entered was correct
+if ($dbbemail == $dbemail) {
+  echo "<script>
+alert('Same User Name Exist! Try Another User Name');
+window.location.href='user_signup.php';
+</script>";
+  exit();
+
+  }
+
+}
+}
+ $sql="INSERT INTO blood_donor(full_name, email, contact_number, gender, age, blood_group, division, address, password)
+VALUES ('$dbfname','$dbemail','$dbcontact','$dbgender','$dbage','$dbbgroup','$dbdivision','$dbaddress','$dbpassword')";
+mysqli_query($conn, $sql);
+header("Location: user_login.php");
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -32,30 +75,30 @@
 	<p class="divider-text">
         <span class="bg-light">OR</span>
     </p>
-	<form>
+	<form action="user_signup.php" id="signform" method="POST">
 	<div class="form-group input-group">
 		<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
 		 </div>
-        <input name="" class="form-control" placeholder="Full name" type="text" required="">
+        <input name="fullname" class="form-control" placeholder="Full name" type="text" required="">
     </div>
     <div class="form-group input-group">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
 		 </div>
-        <input name="" class="form-control" placeholder="Email address" type="email" required="">
+        <input name="email" class="form-control" placeholder="Email address" type="email" required="">
     </div>
     <div class="form-group input-group">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-phone">  </i> </span>
 		</div>
-    	<input name="" class="form-control" placeholder="Phone number" type="text"  required="">
+    	<input name="contact" class="form-control" placeholder="Phone number" type="text"  required="">
     </div>
     <div class="form-group input-group">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"    >  <i class="fa fa-venus-mars"></i> </span>
 		</div>
-		<select class="form-control"  required="" >
+		<select name="gender" class="form-control"  required="" >
 			<option selected="" disabled selected> Select Gender</option>
 			<option>Male</option>
 			<option>Female</option>
@@ -63,11 +106,17 @@
 
 		</select>
 	</div>
+  <div class="form-group input-group">
+    <div class="input-group-prepend">
+      <span class="input-group-text"> <i class="fa fa-calendar"></i> </span>
+  </div>
+      <input name="age" class="form-control" placeholder="Age" type="text" required="">
+  </div>
     <div class="form-group input-group">
     	<div class="input-group-prepend">
-		    <span class="input-group-text"    >  <i class="fa fa-medkit"></i> </span>
+		    <span class="input-group-text" >  <i class="fa fa-medkit"></i> </span>
 		</div>
-		<select class="form-control"  required="" >
+		<select name="bgroup" class="form-control"  required="" >
 			<option selected="" disabled selected> Select Blood Group</option>
 			<option> A+</option>
 			<option> A-</option>
@@ -82,9 +131,9 @@
 	</div>
   <div class="form-group input-group">
     <div class="input-group-prepend">
-      <span class="input-group-text"    >  <i class="fa fa-map-marker"></i> </span>
+      <span class="input-group-text">  <i class="fa fa-map-marker"></i> </span>
   </div>
-  <select class="form-control"  required="" >
+  <select name="division" class="form-control"  required="" >
     <option selected="" disabled selected>Select Division</option>
     <option>Barishal</option>
     <option>Chittagong</option>
@@ -101,17 +150,17 @@
     <div class="input-group-prepend">
       <span class="input-group-text"> <i class="fa fa-location-arrow"></i> </span>
   </div>
-      <input class="form-control" placeholder="Address" type="text" required="">
+      <input name="address" class="form-control" placeholder="Address" type="text" required="">
   </div>
     <div class="form-group input-group">
     	<div class="input-group-prepend">
 		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
 		</div>
-        <input class="form-control" placeholder="Create password" type="password" required=" ">
+        <input name="password"class="form-control" placeholder="Create password" type="password" required=" ">
     </div>
 
     <div class="form-group">
-        <button type="submit" class="btn btn-primary btn-block"> Create Account  </button>
+        <button type="submit" name="signup" form="signform" class="btn btn-primary btn-block"> Create Account  </button>
     </div>
     <p class="text-center">Have an account? <a href="">Log In</a> </p>
 </form>
