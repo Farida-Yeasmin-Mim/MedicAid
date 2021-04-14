4517ccc79ca0d231a9eb93d5264338e21052ce3b
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-04-14 17:42:34 +0600

    Update user_profile.php

--- MedicAid+/user_profile.php
+++ MedicAid+/user_profile.php
@@ -1,3 +1,8 @@
+<?php
+include 'connection.php';
+$dbid =  $_GET['id'];
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -9,21 +14,122 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
+
   </head>
   <body>
     <div class="container-fluid">
       <h1 class="text-center text-dark text-capitalize pt-5">About Us</h1>
       <hr class="w-25 pt-5">
-      <div class="card mx-auto" style="width:400px">
 
-  <div class="card-body mx-auto">
-    <h4 class="card-title">John Doe</h4>
-    <p class="card-text">Some example text.</p>
-    <a href="#" class="btn btn-primary">See Profile</a>
-  </div>
+<div class="container bootstrap snippets bootdey">
+<div class="panel-body inf-content ">
+    <div class="row">
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+
+            <div class="table-responsive ">
+            <table class="table table-user-information">
+                <tbody>
+                    <tr>
+                        <td>
+                            <strong >
+
+                                Identificacion
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                            123456789
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-user  text-primary"></span>
+                                Name
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                            Bootdey
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-cloud text-primary"></span>
+                                Lastname
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                            Bootstrap
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-bookmark text-primary"></span>
+                                Username
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                            bootnipets
+                        </td>
+                    </tr>
+
+
+                    <tr>
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-eye-open text-primary"></span>
+                                Role
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                            Admin
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-envelope text-primary"></span>
+                                Email
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                            noreply@email.com
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-calendar text-primary"></span>
+                                created
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                            20 jul 20014
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>
+                                <span class="glyphicon glyphicon-calendar text-primary"></span>
+                                Modified
+                            </strong>
+                        </td>
+                        <td class="text-primary">
+                             20 jul 20014 20:00:00
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+    </div>
 </div>
-<hr class="w-50 pt-5">
+</div>
 
 
   </body>
