92a82c21966e70453f053fa4eefc424d9fd30199
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-04-18 22:09:03 +0600

    Update contactus.php

--- MedicAid+/contactus.php
+++ MedicAid+/contactus.php
@@ -25,11 +25,11 @@
         <div class="team-members">
 
           <div class="member">
-            <img src="Image/member1.jpg">
+            <img src="Image/member2.jpg">
             <div class="inner">
               <div class="info">
-                <h5>Kazi Ramisa Rifa</h5>
-                <p>Designer</p>
+                <h5>Farida Yeasmin Mim</h5>
+                <p>Developer</p>
                 <div class="social-links">
                   <a href="https://www.facebook.com/yeasminmim"><span class="fa fa-facebook"></span></a>
                   <a href=""><span class="fa fa-instagram"></span></a>
@@ -41,11 +41,11 @@
           </div>
 
           <div class="member">
-            <img src="Image/member2.jpg">
+            <img src="Image/member1.jpg">
             <div class="inner">
               <div class="info">
-                <h5>Farida Yeasmin Mim</h5>
-                <p>Designer</p>
+                <h5>Kazi Ramisa Rifa</h5>
+                <p>Developer</p>
                 <div class="social-links">
                   <a href="https://www.facebook.com/ramisa.rifa01/"><span class="fa fa-facebook"></span></a>
                   <a href=""><span class="fa fa-instagram"></span></a>
@@ -61,7 +61,7 @@
             <div class="inner">
               <div class="info">
                 <h5>Zarin Tasnim</h5>
-                <p>Designer</p>
+                <p>Developer</p>
                 <div class="social-links">
                   <a href="https://www.facebook.com/abidfahim.bd"><span class="fa fa-facebook"></span></a>
                   <a href=""><span class="fa fa-instagram"></span></a>
