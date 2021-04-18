b9fd32d12e889b928c988803c00c865d44246c47
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-04-18 22:28:02 +0600

    Update volunteer.php

--- MedicAid+/volunteer.php
+++ MedicAid+/volunteer.php
@@ -5,13 +5,6 @@
     <title>Volunteer</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -19,9 +12,7 @@
   <link rel="stylesheet" href="CSS/volunteer.css">
   </head>
   <body>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
 <!--Mask-->
 <div id="intro" class="view">
@@ -37,7 +28,7 @@
                     <!-- Heading -->
                     <h2 class="display-4 font-weight-bold text-white pt-5 mb-2">Become a Volunteer</h2>
 
-                  
+
                     <!-- Description -->
                     <h4 class="text-white my-4">Your time and talent can make a real difference in people’s lives.</h4>
 
@@ -66,7 +57,6 @@
 
         <!--Grid row-->
         <div class="row d-flex justify-content-center mb-4">
-
             <!--Grid column-->
             <div class="col-md-8">
 
@@ -77,12 +67,12 @@
                     facere esse molestiae ipsa labore porro minima quam quaerat rem, natus repudiandae debitis
                     est
                     sit pariatur.</p>
-
             </div>
             <!--Grid column-->
-
         </div>
         <!--Grid row-->
+
+
 
         <!--Grid row-->
         <div class="row">
