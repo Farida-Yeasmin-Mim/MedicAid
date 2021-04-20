e9cadd96ea07e93ac67cdda6ffd2a6305e64fefd
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-04-21 03:02:06 +0600

    Update information.php

--- MedicAid+/information.php
+++ MedicAid+/information.php
@@ -1,1 +1,119 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html>
+<head>
+  <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  
+    <link rel="stylesheet" href="information.css">
+
+    <!--a href="https://www.vectorstock.com/royalty-free-vector/young-man-gives-blood-medical-vector-31652142">Young man gives blood medical vector by vectorstock</a-->
+    <!--a href='https://www.freepik.com/photos/background'>Background photo created by kbza - www.freepik.com</a-->
+</head>
+<body>
+
+
+    <!--header-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <a class="navbar-brand" href="#">
+    <img src="Image/Main Logo.jpg" alt="Logo" style="width:153px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Donation</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Ambulance</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+  <!--header end-->
+
+
+
+  <div class="card">
+    <img class="card-image" src="Image/information2.jpg"  alt="Card image" >
+  <div class="card-img-overlay">
+    <br>
+    <br>
+    <br>
+    
+
+  <div class="container">
+    <div class="row">
+
+     
+      <div class="col-sm-4 py-3 py-sm-0">
+        <div class="card box-shadow">
+          <img src="Image/information4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-primary">Blood Donation Process</h5>
+            <p class="card-text text-dark">The blood donation process from the time you arrive until the time you leave takes about an hour. The donation itself is only about 8-10 minutes on average.</p>
+            <a href="#" class="btn btn-primary">Read more...</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 py-3 py-sm-0">
+        <div class="card box-shadow">
+          <img src="Image/information3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-primary">How to Donate</h5>
+            <p class="card-text text-dark">To ensure safety, these are some of the requirements donors must meet to be eligible.To see a list of eligibility information,search donating requirements.</p>
+            <a href="#" class="btn btn-primary">Read more...</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 py-3 py-sm-0">
+        <div class="card box-shadow">
+          <img src="Image/information.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-primary">Common Concerns</h5>
+            <p class="card-text">Many people are reluctant to make their first blood donation, but once they do, they find the donation process to be easy and gratifying.</p>
+            <a href="#" class="btn btn-primary">Read more...</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <br>
+
+
+  
+
+ 
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
 
