d7bfaa92e42bd5e409fe539fc517c0095b2ae729
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-04-24 06:25:45 +0600

    Create information1.php
    Blood Donation Process php file

---
+++ MedicAid+/information1.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html>
+
+<head>
+   <meta charset="utf-8">
+  <title></title>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/information1.css">
+
+
+</head>
+<body>
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <a class="navbar-brand" href="#">
+    <img src="Image/Main Logo.jpg" alt="Logo" style="width:153px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Donation</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Ambulance</a>
+      </li>
+    </ul>
+    <form class="form-inline  my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<br>
+<br>
+
+
+
+  <div class="header">
+  <h2>Blood Donation Process</h2>
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>Donation Process Overview</h2>
+      <h5>Registration and Health History</h5> 
+      <!--div class="fakeimg" style="height:200px;">Image</div-->
+      <img src="Image/5266696.jpg" height="900">
+      <p> 1. We’ll sign you in and you will go over Blood Donation eligibility.<br>
+        2. You’ll read some information about donating blood.<br>
+        3.You’ll answer a few questions about your health history and places you’ve traveled, during a private and confidential interview.<br>
+        4.You’ll tell us about any prescription and/or over the counter medications that may be in your system.<br>
+        5.We’ll check your temperature, pulse, blood pressure and hemoglobin level.</p>
+    </div>
+
+
+    <div class="card">
+      <h2>Donation and Recovery</h2>
+      
+      <!--div class="fakeimg" style="height:200px;">Image</div-->
+      <img src="Image/information1.jpg" height="900">
+      <p>If you’re donating whole blood, we’ll cleanse an area on your arm and insert a brand new sterile needle for the blood draw. (This feels like a quick pinch and is over in seconds.)<br>
+Other types of donations, such as platelets, are made using an apheresis machine which will be connected to both arms.<br>
+A whole blood donation takes about 8-10 minutes, during which you’ll be seated comfortably or lying down.<br>
+When approximately a pint of whole blood has been collected, the donation is complete and a staff person will place a bandage on your arm.<br>
+For platelets, the apheresis machine will collect a small amount of blood, remove the platelets, and return the rest of the blood through your other arm; this cycle will be repeated several times over about 2 hours.<br>
+<br>
+After donating blood, you’ll have a snack and something to drink in the refreshment area.<br>
+You’ll leave after 10-15 minutes and continue your normal routine.<br>
+Enjoy the feeling of accomplishment knowing you are helping to save lives.<br>
+Take a selfie, or simply share your good deed with friends. It may inspire them to become blood donors.
+  </p>
+    </div>
+
+  
+
+</div>
+  
+  </div>
+</div>
+
+
+
+</body>
+</html>
+
+
+
+
