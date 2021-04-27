1ef5206259d58f0a74585fb5b08acb8b665cbeed
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-04-27 10:07:04 +0600

    Add files via upload
    Common Concerns

---
+++ MedicAid+/information3.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html>
+
+<head>
+   <meta charset="utf-8">
+  <title></title>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/information3.css">
+
+
+</head>
+<body>
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <a class="navbar-brand" href="#">
+    <img src="Image/Main Logo.jpg" alt="Logo" style="width:153px"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Donation</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Ambulance</a>
+      </li>
+    </ul>
+    <form class="form-inline  my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<br>
+<br>
+
+
+
+  <div class="header">
+  <h2>Common Concerns</h2>
+  <p></p>
+  
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>I Don't Like Needles</h2>
+      <h5>You’re not alone: fear of needles certainly prevents some people from donating blood. But if you’re willing to spend a few minutes facing your fears (donating really can be that fast) you may find that the satisfaction you feel makes it worth it.</h5> 
+      <!--div class="fakeimg" style="height:200px;">Image</div-->
+      <!--img src="Image/information20.jpg" height="900"-->
+      <p> Whole Blood Donation:<br>
+Whole blood is the most flexible type of donation. It can be transfused in its original form, or used to help multiple people when separated into its specific components of red cells, plasma and platelets.<br>
+
+Who it helps: Whole blood is frequently given to trauma patients and people undergoing surgery.<br>
+
+Time it takes: About 1 hour<br>
+
+Ideal blood types: All blood types<br>
+
+Donation frequency: Every 56 days</p>
+
+<br>
+<br>
+<br>
+Power Red Donation:<br>
+During a Power Red donation, you give a concentrated dose of red cells, the part of your blood used every day for those needing transfusions as part of their care. This type of donation uses an automated process that separates your red blood cells from the other blood components, and then safely and comfortably returns your plasma and platelets to you. <br>
+
+With just a little extra time at your appointment, you can donate more red cells and increase your impact on patients in need. Learn more about Power Red donations.
+Who it helps: Red cells from a Power Red donation are typically given to trauma patients, newborns and emergency transfusions during birth, people with sickle cell anemia, and anyone suffering blood loss.<br>
+
+Time it takes: About 1.5 hours<br>
+
+Ideal blood types: O positive, O negative, A negative, and B negative<br>
+
+Donation frequency: Every 112 days, up to 3 times/year. <br>
+<br>
+<br>
+<br>
+Platelet Donation: <br>
+Platelets are tiny cells in your blood that form clots and stop bleeding. Platelets are most often used by cancer patients and others facing life-threatening illnesses and injuries.<br>
+
+In a platelet donation, an apheresis machine collects your platelets along with some plasma, returning your red cells and most of the plasma back to you. A single donation of platelets can yield several transfusable units, whereas it takes about five whole blood donations to make up a single transfusable unit of platelets.<br>
+
+Platelets are collected at Red Cross donation centers only, and are not collected at blood drives. Learn more about platelet donations.<br>
+Who it helps: Platelets are a vital element of cancer treatments and organ transplant procedures, as well as other surgical procedures.<br>
+Time it takes: About 2.5-3 hours<br>
+
+Ideal blood types: A positive, A negative, B positive, O positive, AB positive and AB negative<br>
+
+Donation frequency: Every 7 days, up to 24 times/year<br>
+<br>
+<br>
+<br>
+Plasma Donation:<br>
+During an AB Elite donation, you give plasma, a part of your blood used to treat patients in emergency situations. AB plasma can be given to anyone regardless of their blood type. Plasma is collected through an automated process that separates plasma from other blood components, then safely and comfortably returns your red blood cells and platelets to you. AB Elite maximizes your donation and takes just a few minutes longer than donating blood.<br>
+
+Plasma is collected at select Red Cross donation centers. Learn more about plasma donation.<br>
+Who it helps: AB Plasma is used in emergency and trauma situations to help stop bleeding. <br>
+
+Time it takes: About 1 hour and 15 minutes<br>
+
+Ideal blood types: AB positive, AB negative<br>
+
+Donation frequency: Every 28 days, up to 13 times/year<br>
+<br>
+
+
+    </div>
+
+
+    <div class="card">
+      <h2>How Blood Donation help</h2>
+      
+      <!--div class="fakeimg" style="height:200px;">Image</div-->
+      <!--img src="Image/information21.jpg" height="900"-->
+      <p>If you’re donating whole blood, we’ll cleanse an area on your arm and insert a brand new sterile needle for the blood draw. (This feels like a quick pinch and is over in seconds.)<br>
+Other types of donations, such as platelets, are made using an apheresis machine which will be connected to both arms.<br>
+A whole blood donation takes about 8-10 minutes, during which you’ll be seated comfortably or lying down.<br>
+When approximately a pint of whole blood has been collected, the donation is complete and a staff person will place a bandage on your arm.<br>
+For platelets, the apheresis machine will collect a small amount of blood, remove the platelets, and return the rest of the blood through your other arm; this cycle will be repeated several times over about 2 hours.<br>
+<br>
+After donating blood, you’ll have a snack and something to drink in the refreshment area.<br>
+You’ll leave after 10-15 minutes and continue your normal routine.<br>
+Enjoy the feeling of accomplishment knowing you are helping to save lives.<br>
+Take a selfie, or simply share your good deed with friends. It may inspire them to become blood donors.
+  </p>
+    </div>
+
+  
+
+</div>
+  
+  </div>
+</div>
+
+
+
+</body>
+</html>
+
+
+
+
+
