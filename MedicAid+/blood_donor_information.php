087e6b456dc745bf91cce940c914116436337d33
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-02 00:26:28 +0600

    Update blood_donor_information.php
    Updating some features to get information of donor easily

--- MedicAid+/blood_donor_information.php
+++ MedicAid+/blood_donor_information.php
@@ -10,8 +10,16 @@ $result = mysqli_query($conn, "SELECT * FROM blood_donor");
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
 <title>Image Upload</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
 <link rel="stylesheet" href="CSS/blood_donor_information.css">
+
 
 <!-- Bootstrap Grid System-->
 <link rel="stylesheet" href="CSS/bootstrap-grid.css">
@@ -21,13 +29,18 @@ $result = mysqli_query($conn, "SELECT * FROM blood_donor");
 </head>
 <body>
   <header>
-    <?php include 'header.php';?>
-    <style>
-h1 {
-  text-align: center;
-}</style>
-    <h1> Recipes </h1>
+    <?php include 'header2.php';?>
+
               </header>
+
+                <div class="container-fluid">
+                  <h1 class="text-center text-dark text-capitalize pt-5">Donor Information</h1>
+                  <hr class="w-25 pt-5">
+
+              <form class="example" action="index.php">
+  <input type="text" placeholder="Search.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
 <div id="content">
   <?php
     while ($row = mysqli_fetch_array($result)) {
@@ -40,7 +53,6 @@ h1 {
         echo "<h6>Blood Group: ".$row['blood_group']." </h6>";
         echo "<h6>Division: ".$row['division']." </h6>";
         echo "<h6>Address: ".$row['address']." </h6>";
-      	echo "<p> </p>";
 
 
       echo "</div>";
