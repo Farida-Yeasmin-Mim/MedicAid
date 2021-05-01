8f7237bbbcd8a3a7a882fc5feee51ab4ff3392e4
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-02 05:05:16 +0600

    Update feedback.php

--- MedicAid+/feedback.php
+++ MedicAid+/feedback.php
@@ -1,34 +1,97 @@
-<div class="container-fluid bg-grey">
-  <h2 class="text-center">CONTACT</h2>
-  <div class="row">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Feedback</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/feedback.css">
+  </head>
+  <body>
 
-    <div class="col-sm-5">
-      <p>Contact us and we'll get back to you within 24 hours.</p>
-      <p><span class="glyphicon glyphicon-map-marker"></span> Dhaka, Bangladesh</p>
-      <p><span class="glyphicon glyphicon-phone"></span> +00 1515151515</p>
-      <p><span class="glyphicon glyphicon-envelope"></span> myemail@something.com</p>
-    </div>
 
-    <div class="col-sm-7">
-      <div class="row">
-        <div class="col-sm-6 form-group">
-          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+    <!--Section: Contact-->
+    <section id="contact">
+
+        <!-- Heading -->
+        <h2 class="mb-5 font-weight-bold text-center">Send us your Feedback!</h2>
+
+        <!--Grid row-->
+        <div class="row">
+
+            <!--Grid column-->
+            <div class="col-lg-5 col-md-12 ">
+                <!-- Form contact -->
+                <form class="p-5 grey-text">
+                    <div class="md-form form-sm"> <i class="fa fa-user prefix"></i>
+                        <input type="text" id="form3" class="form-control form-control-sm">
+                        <label for="form3">Your name</label>
+                    </div>
+                    <div class="md-form form-sm"> <i class="fa fa-envelope prefix"></i>
+                        <input type="text" id="form2" class="form-control form-control-sm">
+                        <label for="form2">Your email</label>
+                    </div>
+                    <div class="md-form form-sm"> <i class="fa fa-tag prefix"></i>
+                        <input type="text" id="form32" class="form-control form-control-sm">
+                        <label for="form34">Subject</label>
+                    </div>
+                    <div class="md-form form-sm"> <i class="fa fa-pencil prefix"></i>
+                        <textarea type="text" id="form8" class="md-textarea form-control form-control-sm" rows="4"></textarea>
+                        <label for="form8">Your message</label>
+                    </div>
+                    <div class="text-center mt-4">
+                        <button class="btn btn-primary">Send <i class="fa fa-paper-plane-o ml-1"></i></button>
+                    </div>
+                </form>
+                <!-- Form contact -->
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-7 col-md-12">
+
+                <!--Grid row-->
+                <div class="row text-center">
+
+                    <!--Grid column-->
+                    <div class="col-lg-4 col-md-12 mb-3">
+
+                        <p><i class="fa fa-map fa-1x mr-2 grey-text"></i>Bangladesh, DK 1216</p>
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-4 col-md-6 mb-3">
+
+                        <p><i class="fa fa-building fa-1x mr-2 grey-text"></i>Mon - Fri, 8:00-22:00</p>
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-4 col-md-6 mb-3">
+
+                        <p><i class="fa fa-phone fa-1x mr-2 grey-text"></i>+ 01 234 567 89</p>
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Google map-->
+                <div id="map-container" class="z-depth-1-half map-container mb-5" style="height: 400px"></div>
+
+            </div>
+            <!--Grid column-->
+
         </div>
+        <!--Grid row-->
 
-        <div class="col-sm-6 form-group">
-          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
-        </div>
-      </div>
-
-      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
-      
-      <div class="row">
-        <div class="col-sm-12 form-group">
-          <button class="btn btn-default pull-right" type="submit">Send</button>
-        </div>
-      </div>
-
- <img src="map.jpg" style="width:100%">
-    </div>
-  </div>
-</div>
+    </section>
+    <!--Section: Contact-->
