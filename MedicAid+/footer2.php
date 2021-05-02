d70f94dc0588648d370a06981916c5dc1c227da8
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-03 05:32:30 +0600

    Create another footer2.php

---
+++ MedicAid+/footer2.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Volunteer</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/volunteer.css">
+  </head>
+  <body>
+
+
+<!-- Footer -->
+<footer class="page-footer font-small bg-light" >
+
+<!-- Social buttons -->
+<div class="bg-dark">
+    <div class="container">
+        <!--Grid row-->
+        <div class="row py-4 d-flex align-items-center">
+
+            <!--Grid column-->
+            <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                <h5 class="mb-0 text-white">Get connected with us on social networks!</h5>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-6 col-lg-7 text-center text-md-right">
+                <!--Facebook-->
+                <a class="fb-ic ml-0">
+                  <i class="fa fa-facebook fa-2x text-blue mr-4"> </i>
+                </a>
+                <!--Twitter-->
+                <a class="tw-ic">
+                    <i class="fa fa-twitter fa-2x text-blue mr-4"> </i>
+                </a>
+                <!--Instagram-->
+                <a class="ins-ic">
+                    <i class="fa fa-instagram fa-2x text-danger mr-lg-4"> </i>
+                </a>
+                <!--Google +-->
+                <a class="gplus-ic">
+                    <i class="fa fa-google-plus fa-2x text-blue mr-4"> </i>
+                </a>
+                <!--Linkedin-->
+                <a class="li-ic">
+                    <i class="fa fa-linkedin fa-2x text-blue mr-4"> </i>
+                </a>
+
+            </div>
+            <!--Grid column-->
+
+        </div>
+        <!--Grid row-->
+    </div>
+</div>
+<!-- Social buttons -->
+
+
+
+<!--Footer Links-->
+<div class="container mt-5 mb-4 text-center text-md-left">
+    <div class="row mt-3">
+
+        <!--First column-->
+        <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+            <h5 class="text-uppercase font-weight-bold">
+                <strong>MedicAid</strong>
+            </h5>
+            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit.</p>
+        </div>
+        <!--/.First column-->
+
+        <!--Second column-->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h5 class="text-uppercase font-weight-bold">
+                <strong>Departments</strong>
+            </h5>
+            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>
+                <a href="#!">MDBootstrap</a>
+            </p>
+            <p>
+                <a href="#!">MDWordPress</a>
+            </p>
+            <p>
+                <a href="#!">BrandFlow</a>
+            </p>
+            <p>
+                <a href="#!">Bootstrap Angular</a>
+            </p>
+        </div>
+        <!--/.Second column-->
+
+        <!--Third column-->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h5 class="text-uppercase font-weight-bold">
+                <strong>Useful links</strong>
+            </h5>
+            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>
+                <a href="#!">Home</a>
+            </p>
+            <p>
+                <a href="#!">About</a>
+            </p>
+            <p>
+                <a href="#!">Volunteer</a>
+            </p>
+            <p>
+                <a href="#!">Appointment</a>
+            </p>
+            <p>
+                <a href="#!">Donation</a>
+            </p>
+            <p>
+                <a href="#!">Ambulance</a>
+            </p>
+
+
+        </div>
+        <!--/.Third column-->
+
+        <!--Fourth column-->
+        <div class="col-md-4 col-lg-3 col-xl-3">
+            <h5 class="text-uppercase font-weight-bold">
+                <strong>Contact</strong>
+            </h5>
+            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>
+                <i class="fa fa-home"></i></i> Bangladesh, Dhaka 1216</p>
+            <p>
+                <i class="fa fa-envelope mr-3"></i> info@example.com</p>
+            <p>
+                <i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+            <p>
+                <i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+        </div>
+        <!--/.Fourth column-->
+
+    </div>
+</div>
+<!--/.Footer Links-->
+
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  © 2021 Copyright :
+  <a class="text-dark" href="">MedicAid+, All Rights Reserved</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+  </body>
+</html>
