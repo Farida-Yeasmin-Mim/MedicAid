18ba7dc9120bd2e710db53a946cb407018396ec1
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-04 02:46:09 +0600

    Update about.php

--- MedicAid+/about.php
+++ MedicAid+/about.php
@@ -2,9 +2,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>About</title>
+    <title>About Us</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/volunteer.css">
   </head>
   <body>
-<h1> This is MedicAid+ </h1>
-  </body>
-</html>
