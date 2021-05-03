3ce45ae1d185b30e5be743514a0110b9b456936d
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-03 21:11:24 +0600

    Update user_profile.php

--- MedicAid+/user_profile.php
+++ MedicAid+/user_profile.php
@@ -1,8 +1,8 @@
-<?php
-include 'connection.php';
-$dbid =  $_GET['id'];
+<!<?php
+//include 'connection.php';
+//$dbid =  $_GET['id'];
 
-?>
+?>>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -11,126 +11,152 @@ $dbid =  $_GET['id'];
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
   </head>
   <body>
-    <div class="container-fluid">
-      <h1 class="text-center text-dark text-capitalize pt-5">About Us</h1>
-      <hr class="w-25 pt-5">
 
-<div class="container bootstrap snippets bootdey">
-<div class="panel-body inf-content ">
-    <div class="row">
-        <div class="col-md-4">
+    <header>
+      <?php include 'header.php';?>
 
+                </header>
+
+    <div class="container emp-profile">
+            <form method="post">
+                <div class="row">
+
+                    <div class="col-md-6 mx-auto">
+                        <div class="profile-head">
+                          <br> <br>
+                                    <h5>
+                                        Kshiti Ghelani
+                                    </h5>
+                                    <h6>
+                                        Web Developer and Designer
+                                    </h6>
+                                    <p class="proile-rating">RANKINGS : <span>8/10</span></p>
+
+
+                            <ul class="nav nav-tabs " id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                      <br><br>
+                        <button class="btn bg-primary text-white">Edit Profile</button>
+                    </div>
+                </div>
+                <div class="row ">
+
+                    <div class=" col-md-6  mx-auto">
+                        <div class="tab-content profile-tab" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                              <br>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>User Id</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Kshiti123</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Name</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Kshiti Ghelani</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Email</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>kshitighelani@gmail.com</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Phone</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>123 456 7890</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Profession</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Web Developer and Designer</p>
+                                            </div>
+                                        </div>
+                            </div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                              <br>
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Experience</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Expert</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Hourly Rate</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>10$/hr</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Total Projects</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>230</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>English Level</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Expert</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Availability</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>6 months</p>
+                                            </div>
+                                        </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label>Your Bio</label><br/>
+                                        <p>Your detail description</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div class="col-md-4">
-
-            <div class="table-responsive ">
-            <table class="table table-user-information">
-                <tbody>
-                    <tr>
-                        <td>
-                            <strong >
-
-                                Identificacion
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                            123456789
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <strong>
-                                <span class="glyphicon glyphicon-user  text-primary"></span>
-                                Name
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                            Bootdey
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <strong>
-                                <span class="glyphicon glyphicon-cloud text-primary"></span>
-                                Lastname
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                            Bootstrap
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <strong>
-                                <span class="glyphicon glyphicon-bookmark text-primary"></span>
-                                Username
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                            bootnipets
-                        </td>
-                    </tr>
-
-
-                    <tr>
-                        <td>
-                            <strong>
-                                <span class="glyphicon glyphicon-eye-open text-primary"></span>
-                                Role
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                            Admin
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <strong>
-                                <span class="glyphicon glyphicon-envelope text-primary"></span>
-                                Email
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                            noreply@email.com
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <strong>
-                                <span class="glyphicon glyphicon-calendar text-primary"></span>
-                                created
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                            20 jul 20014
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <strong>
-                                <span class="glyphicon glyphicon-calendar text-primary"></span>
-                                Modified
-                            </strong>
-                        </td>
-                        <td class="text-primary">
-                             20 jul 20014 20:00:00
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-
 
   </body>
 </html>
