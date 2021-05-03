5416bb98f7cd616473e634d1b90ccf24637494ca
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-03 22:03:41 +0600

    Bug Fixing
    Icon not showing bug fixed

--- MedicAid+/user_login.php
+++ MedicAid+/user_login.php
@@ -1,6 +1,40 @@
 <?php
     require_once "config.php";
 	$loginURL = $gClient->createAuthUrl();
+
+  include 'connection.php';
+
+  if (isset($_POST['log_in'])) {
+      // Set variables to represent data from database
+      $dbUsname = strip_tags($_POST['username']);
+  $dbEmail = strip_tags($_POST['email']);
+  $dbpass = strip_tags($_POST['password']);
+
+
+      $sql = "SELECT User_Name, Email, Password FROM user";
+  $result = mysqli_query($conn,$sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+      $UserName= $row["User_Name"];
+      $Email= $row["Email"] ;
+      $Pass= $row["Password"] ;
+      // Check if the username and the password they entered was correct
+      if ($UserName == $dbUsname && $Email == $dbEmail && $Pass == $dbpass) {
+    header("Location: user_profile.php?id=$UserName");
+      } else {
+      // Display the alert box
+      echo "<script>
+    alert('Invalid Input!');
+    window.location.href='user_login.php';
+    </script>";
+  }
+
+    }
+  }
+
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -12,8 +46,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
   <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
   <link rel="stylesheet" href="CSS/user_login.css">
   <style>
 body {
@@ -24,6 +60,7 @@ body {
 }
 </style>
   </head>
+
   <body>
     <header>
       <?php include 'header.php';?>
@@ -33,7 +70,7 @@ body {
       <h1 class="text-center text-dark text-capitalize pt-5">Login</h1>
       <hr class="w-25 pt-4">
       <div class="w-25 mx-auto">
-        <form action="/action_page.php">
+        <form id="loginform" method="POST" action="user_login.php">
 
   <div class="form-group">
     <label for="email">Email address:</label>
@@ -46,11 +83,12 @@ body {
   </div>
 
   <div class="col-md-12 text-center">
-  <button type="submit" class="btn btn-primary w-50">Login</button>
+  <button name="log_in" class="btn btn-primary w-50"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</button>
   <hr>
-  <button type="submit" class="btn btn-danger ">Sign Up</button>
-  <button type="button" onclick="window.location = '<?php echo $loginURL ?>';" class="btn btn-danger"><i class="fa fa-google"></i>Login With Google</button>
-  
+  <button type="submit" class="btn btn-danger "><i class="fa fa-user-plus" aria-hidden="true"></i>
+Sign Up</button>
+  <button type="button" onclick="window.location = '<?php echo $loginURL ?>';" class="btn btn-danger"><i class="fa fa-google"></i> Login With Google</button>
+
 </div>
 
 </form>
