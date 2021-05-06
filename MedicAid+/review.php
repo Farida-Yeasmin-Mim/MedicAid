fbc01a19b55503317b89dfce83a4abdbd3668fad
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-07 04:52:17 +0600

    Creating A Review Page To See User Reviews

---
+++ MedicAid+/review.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Reviews</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+
+    <div class="container-fluid">
+      <h1 class="text-center text-dark text-capitalize pt-5">Reviews</h1>
+      <hr>
+      <div id="content">
+        <div id='img_div'>
+    <div class="media">
+  <img src="Image/avatar.jpg" class="align-self-start mr-3" style="width:60px">
+  <div class="media-body">
+    <h4>Ramisa</h4>
+    <p>It is very useful software...</p>
+  </div>
+</div>
+</div>
+</div>
+  </body>
+</html>
