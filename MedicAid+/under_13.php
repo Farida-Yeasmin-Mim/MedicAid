d64a13e407af7cc3dafee37f1dd7223687a317d9
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-07 03:11:27 +0600

    Create under_13.php

---
+++ MedicAid+/under_13.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Under age 13</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/appointment.css">
+
+  <style>
+body {
+  background-image: url('Image/baby.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+
+  </head>
+<body>
+
+
+    <section class="ftco-counter img ftco-section ftco-no-pt ftco-no-pb">
+    	<div class="container">
+
+    			<div class=" col-md-4 col-lg-5 pl-lg-5 py-md-2">
+	    				<div class="row justify-content-start pb-3">
+			          <div class="col-md-20 heading-section ftco-animate p-4 p-lg-5">
+			            <h1 class="mb-5 text-center"><strong>Children's Online Privacy Protection</strong></h1>
+			            <h5><p class="text-center text-danger">In accordance with the Children's Online Privacy Protection Act of 1998,
+                    youth under the age of 13 cannot be registered on MedicAid websites nor
+                    have personal information stored online by any MedicAid unit, but can still support the organization and
+                    be recognized as a volunteer. Interested youth volunteers under the age of 13 must contact the local unit
+                    to complete a parental consent form and be accompanied by an adult for supervision.</p></h5>
+			          </div>
+			        </div>
+        </div>
+    	</div>
+    </section>
