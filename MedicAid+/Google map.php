5f1e78fa1ffa2e12795e2313db7a3bc249702b25
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-09 07:18:42 +0600

    Update Google map.php

--- MedicAid+/Google map.php
+++ MedicAid+/Google map.php
@@ -21,3 +21,27 @@
 </body>
 </html>
 
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="googlemap.js"></script>
+	<style type="text/css">
+		
+		.container{
+			height: 450px;
+		}
+		#map{
+			width: 100%;
+			height:100%;
+			border: 1px solid blue;
+		}
+	</style>
+
+
+
