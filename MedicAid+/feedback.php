0ac6ec4f424340f04b50846dbab470afc1cc37b3
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-13 16:40:42 +0600

    Feedback Page design Update

--- MedicAid+/feedback.php
+++ MedicAid+/feedback.php
@@ -10,85 +10,62 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="CSS/feedback.css">
+  <style>
+  body {
+  background-image: url('Image/fbbg.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  }
+  </style>
   </head>
   <body>
+    <header>
+      <?php include 'header2.php';?>
 
+                </header>
 
     <!--Section: Contact-->
     <section id="contact">
 
         <!-- Heading -->
-        <h2 class="mb-5 font-weight-bold text-center">Send us your Feedback about MedicAid!</h2>
+        <div class="container-fluid">
+          <h2 class="text-center font-weight-bold text-dark text-capitalize pt-5">Send us your Feedback about MedicAid!</h2>
+          <hr class="w-25">
+
 
         <!--Grid row-->
-        <div class="row">
+        <div class="row" >
 
             <!--Grid column-->
-            <div class="col-lg-5 col-md-12 ">
+            <div class="col-lg-5 col-md-12 mx-auto">
                 <!-- Form contact -->
-                <form class="p-5 grey-text">
-                    <div class="md-form form-sm"> <i class="fa fa-user prefix"></i>
+                <form class="p-5 grey-text ">
+                    <div class="md-form form-sm"> Your name <i class="fa fa-user prefix"> </i>
                         <input type="text" id="form3" class="form-control form-control-sm">
-                        <label for="form3">Your name</label>
+
                     </div>
-                    <div class="md-form form-sm"> <i class="fa fa-envelope prefix"></i>
+                    <br>
+                    <div class="md-form form-sm"> Your Email <i class="fa fa-envelope prefix"></i>
                         <input type="text" id="form2" class="form-control form-control-sm">
-                        <label for="form2">Your email</label>
+
                     </div>
-                    <div class="md-form form-sm"> <i class="fa fa-tag prefix"></i>
-                        <input type="text" id="form32" class="form-control form-control-sm">
-                        <label for="form34">Subject</label>
-                    </div>
-                    <div class="md-form form-sm"> <i class="fa fa-pencil prefix"></i>
+                    <br>
+                    <div class="md-form form-sm"> Your message <i class="fa fa-pencil prefix"></i>
                         <textarea type="text" id="form8" class="md-textarea form-control form-control-sm" rows="4"></textarea>
-                        <label for="form8">Your message</label>
+
                     </div>
+                    <br>
                     <div class="text-center mt-4">
                         <button class="btn btn-primary">Send <i class="fa fa-paper-plane-o ml-1"></i></button>
                     </div>
+
                 </form>
                 <!-- Form contact -->
             </div>
             <!--Grid column-->
 
-            <!--Grid column-->
-            <div class="col-lg-7 col-md-12">
 
-                <!--Grid row-->
-                <div class="row text-center">
-
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-12 mb-3">
-
-                        <p><i class="fa fa-map fa-1x mr-2 grey-text"></i>Bangladesh, DK 1216</p>
-
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-6 mb-3">
-
-                        <p><i class="fa fa-building fa-1x mr-2 grey-text"></i>Mon - Fri, 8:00-22:00</p>
-
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-4 col-md-6 mb-3">
-
-                        <p><i class="fa fa-phone fa-1x mr-2 grey-text"></i>+ 01 234 567 89</p>
-
-                    </div>
-                    <!--Grid column-->
-
-                </div>
-                <!--Grid row-->
-
-                <!--Google map-->
-                <div id="map-container" class="z-depth-1-half map-container mb-5" style="height: 400px"></div>
-
-            </div>
-            <!--Grid column-->
 
         </div>
         <!--Grid row-->
