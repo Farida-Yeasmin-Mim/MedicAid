066fbc18b81665b6ac87c5b324d60e671bafcb4b
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-15 01:55:59 +0600

    Review Page Done

--- MedicAid+/review.php
+++ MedicAid+/review.php
@@ -1,3 +1,12 @@
+<?php
+  // Create database connection
+  include 'connection.php';
+
+
+      $sql = "SELECT name,message FROM feedback";
+
+$result = mysqli_query($conn, $sql);
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -8,25 +17,52 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
   <script src="https://apis.google.com/js/platform.js" async defer></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+  body {
+  background-image: url('Image/review.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  }
+  </style>
   </head>
   <body>
+    <header>
+      <?php include 'header.php';?>
+                </header>
+
 
     <div class="container-fluid">
       <h1 class="text-center text-dark text-capitalize pt-5">Reviews</h1>
-      <hr>
+      <hr class="w-25 pt-5">
+      <!--Grid row-->
+      <div class="row" >
+
+          <!--Grid column-->
+          <div class="col-lg-5 col-md-12 mx-auto">
+
+
       <div id="content">
+        <?php
+          while ($row = mysqli_fetch_array($result)) { ?>
         <div id='img_div'>
+
     <div class="media">
+
   <img src="Image/avatar.jpg" class="align-self-start mr-3" style="width:60px">
   <div class="media-body">
-    <h4>Ramisa</h4>
-    <p>It is very useful software...</p>
+    <h4><?php echo $row['name'] ?></h4>
+    <p><?php echo $row['message'] ?></p>
+    <hr class="w-100">
   </div>
 </div>
 </div>
+<?php } ?>
 </div>
+</div>
+</div>
+
   </body>
 </html>
