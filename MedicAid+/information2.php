860fb5fca66afb9a5ebaa000344a2e3bc7a2f6d8
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-16 04:53:52 +0600

    Update information2.php

--- MedicAid+/information2.php
+++ MedicAid+/information2.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<DOCTYPE html>
 <html lang="en" dir="ltr">
 <html>
 
@@ -8,43 +8,20 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/information1.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    
 
 
 </head>
+<header>
+     
+   <?php include 'header.php';?>
+
+  </header>
 <body>
 
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <a class="navbar-brand" href="#">
-    <img src="Image/Main Logo.jpg" alt="Logo" style="width:153px"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Donation</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Ambulance</a>
-      </li>
-    </ul>
-    <form class="form-inline  my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-<br>
-<br>
-
+  
 
 
   <div class="header">
@@ -60,59 +37,59 @@
       <!--div class="fakeimg" style="height:200px;">Image</div-->
       <img src="Image/information20.jpg" height="900">
       <p> Whole Blood Donation:<br>
-Whole blood is the most flexible type of donation. It can be transfused in its original form, or used to help multiple people when separated into its specific components of red cells, plasma and platelets.<br>
+<i class="fas fa-check"style="color:blue"></i>Whole blood is the most flexible type of donation. It can be transfused in its original form, or used to help multiple people when separated into its specific components of red cells, plasma and platelets.<br>
 
-Who it helps: Whole blood is frequently given to trauma patients and people undergoing surgeries.<br>
+<i class="fas fa-check"style="color:blue"></i>Who it helps: Whole blood is frequently given to trauma patients and people undergoing surgeries.<br>
 
-Time it takes: About 1 hour<br>
+<i class="fas fa-check"style="color:blue"></i>Time it takes: About 1 hour<br>
 
-Ideal blood types: All blood types<br>
+<i class="fas fa-check"style="color:blue"></i>Ideal blood types: All blood types<br>
 
-Donation frequency: Every 56 days</p>
+<i class="fas fa-check"style="color:blue"></i>Donation frequency: Every 56 days</p>
 
 <br>
 <br>
 <br>
 Power Red Donation:<br>
-During a Power Red donation, you give a concentrated dose of red cells, the part of your blood used every day for those needing transfusions as part of their care. This type of donation uses an automated process that separates your red blood cells from the other blood components, and then safely and comfortably returns your plasma and platelets to you. <br>
+<i class="fas fa-check"style="color:blue"></i>During a Power Red donation, you give a concentrated dose of red cells, the part of your blood used every day for those needing transfusions as part of their care. This type of donation uses an automated process that separates your red blood cells from the other blood components, and then safely and comfortably returns your plasma and platelets to you. <br>
 
-With just a little extra time at your appointment, you can donate more red cells and increase your impact on patients in need. Learn more about Power Red donations.
-Who it helps: Red cells from a Power Red donation are typically given to trauma patients, newborns and emergency transfusions during birth, people with sickle cell anemia, and anyone suffering blood loss.<br>
+<i class="fas fa-check"style="color:blue"></i>With just a little extra time at your appointment, you can donate more red cells and increase your impact on patients in need. Learn more about Power Red donations.
+<i class="fas fa-check"style="color:blue"></i>Who it helps: Red cells from a Power Red donation are typically given to trauma patients, newborns and emergency transfusions during birth, people with sickle cell anemia, and anyone suffering blood loss.<br>
 
-Time it takes: About 1.5 hours<br>
+<i class="fas fa-check"style="color:blue"></i>Time it takes: About 1.5 hours<br>
 
-Ideal blood types: O positive, O negative, A negative, and B negative<br>
+<i class="fas fa-check"style="color:blue"></i>Ideal blood types: O positive, O negative, A negative, and B negative<br>
 
-Donation frequency: Every 112 days, up to 3 times/year. <br>
+<i class="fas fa-check"style="color:blue"></i>Donation frequency: Every 112 days, up to 3 times/year. <br>
 <br>
 <br>
 <br>
 Platelet Donation: <br>
-Platelets are tiny cells in your blood that form clots and stop bleeding. Platelets are most often used by cancer patients and others facing life-threatening illnesses and injuries.<br>
+<i class="fas fa-check"style="color:blue"></i>Platelets are tiny cells in your blood that form clots and stop bleeding. Platelets are most often used by cancer patients and others facing life-threatening illnesses and injuries.<br>
 
-In a platelet donation, an apheresis machine collects your platelets along with some plasma, returning your red cells and most of the plasma back to you. A single donation of platelets can yield several transfusable units, whereas it takes about five whole blood donations to make up a single transfusable unit of platelets.<br>
+<i class="fas fa-check"style="color:blue"></i>In a platelet donation, an apheresis machine collects your platelets along with some plasma, returning your red cells and most of the plasma back to you. A single donation of platelets can yield several transfusable units, whereas it takes about five whole blood donations to make up a single transfusable unit of platelets.<br>
 
-Platelets are collected at Red Cross donation centers only, and are not collected at blood drives. Learn more about platelet donations.<br>
-Who it helps: Platelets are a vital element of cancer treatments and organ transplant procedures, as well as other surgical procedures.<br>
-Time it takes: About 2.5-3 hours<br>
+<i class="fas fa-check"style="color:blue"></i>Platelets are collected at Red Cross donation centers only, and are not collected at blood drives. Learn more about platelet donations.<br>
+<i class="fas fa-check"style="color:blue"></i>Who it helps: Platelets are a vital element of cancer treatments and organ transplant procedures, as well as other surgical procedures.<br>
+<i class="fas fa-check"style="color:blue"></i>Time it takes: About 2.5-3 hours<br>
 
-Ideal blood types: A positive, A negative, B positive, O positive, AB positive and AB negative<br>
+<i class="fas fa-check"style="color:blue"></i>Ideal blood types: A positive, A negative, B positive, O positive, AB positive and AB negative<br>
 
-Donation frequency: Every 7 days, up to 24 times/year<br>
+<i class="fas fa-check"style="color:blue"></i>Donation frequency: Every 7 days, up to 24 times/year<br>
 <br>
 <br>
 <br>
 Plasma Donation:<br>
-During an AB Elite donation, you give plasma, a part of your blood used to treat patients in emergency situations. AB plasma can be given to anyone regardless of their blood type. Plasma is collected through an automated process that separates plasma from other blood components, then safely and comfortably returns your red blood cells and platelets to you. AB Elite maximizes your donation and takes just a few minutes longer than donating blood.<br>
+<i class="fas fa-check"style="color:blue"></i>During an AB Elite donation, you give plasma, a part of your blood used to treat patients in emergency situations. AB plasma can be given to anyone regardless of their blood type. Plasma is collected through an automated process that separates plasma from other blood components, then safely and comfortably returns your red blood cells and platelets to you. AB Elite maximizes your donation and takes just a few minutes longer than donating blood.<br>
 
-Plasma is collected at select Red Cross donation centers. Learn more about plasma donation.<br>
+<i class="fas fa-check"style="color:blue"></i>Plasma is collected at select Red Cross donation centers. Learn more about plasma donation.<br>
 Who it helps: AB Plasma is used in emergency and trauma situations to help stop bleeding. <br>
 
-Time it takes: About 1 hour and 15 minutes<br>
+<i class="fas fa-check"style="color:blue"></i>Time it takes: About 1 hour and 15 minutes<br>
 
-Ideal blood types: AB positive, AB negative<br>
+<i class="fas fa-check"style="color:blue"></i>Ideal blood types: AB positive, AB negative<br>
 
-Donation frequency: Every 28 days, up to 13 times/year<br>
+<i class="fas fa-check"style="color:blue"></i>Donation frequency: Every 28 days, up to 13 times/year<br>
 <br>
 
 
@@ -130,10 +107,10 @@ A whole blood donation takes about 8-10 minutes, during which you’ll be seated
 When approximately a pint of whole blood has been collected, the donation is complete and a staff person will place a bandage on your arm.<br>
 For platelets, the apheresis machine will collect a small amount of blood, remove the platelets, and return the rest of the blood through your other arm; this cycle will be repeated several times over about 2 hours.<br>
 <br>
-After donating blood, you’ll have a snack and something to drink in the refreshment area.<br>
-You’ll leave after 10-15 minutes and continue your normal routine.<br>
-Enjoy the feeling of accomplishment knowing you are helping to save lives.<br>
-Take a selfie, or simply share your good deed with friends. It may inspire them to become blood donors.
+<i class="fas fa-check"style="color:blue"></i>After donating blood, you’ll have a snack and something to drink in the refreshment area.<br>
+<i class="fas fa-check"style="color:blue"></i>You’ll leave after 10-15 minutes and continue your normal routine.<br>
+<i class="fas fa-check"style="color:blue"></i>Enjoy the feeling of accomplishment knowing you are helping to save lives.<br>
+<i class="fas fa-check"style="color:blue"></i>Take a selfie, or simply share your good deed with friends. It may inspire them to become blood donors.
   </p>
     </div>
 
@@ -148,4 +125,5 @@ Take a selfie, or simply share your good deed with friends. It may inspire them 
 
 </body>
 </html>
+
 
