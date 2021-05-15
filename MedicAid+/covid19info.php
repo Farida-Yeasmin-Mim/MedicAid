477e404eed60509c3e057068b29ac95e11e3ec27
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-16 04:40:42 +0600

    Update covid19info.php

--- MedicAid+/covid19info.php
+++ MedicAid+/covid19info.php
@@ -1,4 +1,3 @@
-
 <doctype html>
 <html lang="en">
   <head>
@@ -24,7 +23,16 @@
   </header>
 
   <body>
-    <div class="card-body text-center text-danger ">
+
+   
+    
+  
+
+    
+      
+
+
+<div class="card-body text-center text-danger ">
 
 
 
@@ -38,43 +46,58 @@
 <br>
 <br>
 <br>
-      <p style="color: blue" >Follow these five easy steps to help prevent the spread of COVID-19</p></b>
+    
+
+    
+
+
+
+
+<p style="color: blue" >Follow these five easy steps to help prevent the spread of COVID-19</p></b>
 
 <span class="fa-stack fa-4x">
   <i class="fas fa-circle fa-stack-2x"></i>
   <i class="fas fa-box-tissue fa-stack-1x fa-inverse"></i>
   
 </span>  
-      <p> 1.Sneeze or cough?  Cover your nose and mouth with a tissue or use your elbow. </p>
+ 
+   
+
+<p> 1.Sneeze or cough?  Cover your nose and mouth with a tissue or use your elbow. </p>
 <br>
-      <br>
+<br>
 <span class="fa-stack fa-4x">
   <i class="fas fa-circle fa-stack-2x"></i>
   <i class="fas fa-hands-wash fa-stack-1x fa-inverse"></i>
 </span>
-      <p>2.Wash your hands often with soap and water for at least 20 seconds.</p>
+<p>2.Wash your hands often with soap and water for at least 20 seconds.</p>
 <span class="fa-stack fa-4x">
   <i class="fas fa-square fa-stack-2x"></i>
   <i class="fas fa-pump-medical fa-stack-1x fa-inverse"></i>
 </span>
-      <p>3.Clean and disinfect surfaces around your home and work frequently.</p>
+
+<p>3.Clean and disinfect surfaces around your home and work frequently.</p>
 <span class="fa-stack fa-4x">
   <i class="fas fa-square fa-stack-2x"></i>
   <i class="fas fa-people-arrows fa-stack-1x fa-inverse"></i>
-  
-  /span>
+
+</span>
 <p>4.Keep at least 6 feet between yourself and others if you must be in public.</p>
 
 <span class="fa-stack fa-4x">
   <i class="fas fa-square fa-stack-2x"></i>
   <i class="fas fa-head-side-mask fa-stack-1x fa-inverse"></i>
-  </span>
+ 
+</span>
 <p>5.Wear a cloth face covering over your mouth and nose when around others. </p>
 
 <br>
 <br>
 <br>
-  div class="card">
+
+
+
+<div class="card">
       <h2 style="color: blue"> Help Slow the Spread of COVID-19</h2>
       <br>
       <br>
@@ -93,15 +116,15 @@ Here’s how you can help:<br>
 <i class="fas fa-check-circle"style="color:red"></i>Wear a cloth face covering to cover your mouth and nose when around others. Learn more. <br>
 <i class="fas fa-check-circle"style="color:red"></i>Keep at least 6 feet between you and others if you must be in public. Also avoid:<br>
 <i class="fas fa-check-circle"style="color:red"></i>Close contact with people who do not share your home.<br>
-        <i class="fas fa-check-circle"style="color:red"></i>Group settings, crowded places and mass gatherings. <br>
+<i class="fas fa-check-circle"style="color:red"></i>Group settings, crowded places and mass gatherings. <br>
 <i class="fas fa-check-circle"style="color:red"></i>Order food and other items online for home delivery or curbside pickup (if possible).<br>
 <i class="fas fa-check-circle"style="color:red"></i>Connect with loved ones through video calls, phone calls, texts and social media.<br>
 <i class="fas fa-check-circle"style="color:red"></i>Take precautions if you are at higher risk for severe illness.<br>
 <i class="fas fa-check-circle"style="color:red"></i>If you or someone in your household becomes sick, call a health care provider for medical advice and follow it.<br>
   </p>
     </div>
-      
-      <br>
+
+    <br>
 
     <div class="card">
       <h2 style="color: blue">What to Do If You Are Sick</h2>
@@ -112,11 +135,13 @@ Here’s how you can help:<br>
     
       <p style="color: grey">If you are sick or think you might have COVID-19, follow these tips from the CDC to care for yourself and help protect other people in your home and community. Keep in mind that most people have mild illness and are able to recover at home.<br>
         <br>
-        <i class="fas fa-check"style="color:blue"></i>If You Are Mildly ill, Stay Home and Call a Health Care Provider<br>
+
+<i class="fas fa-check"style="color:blue"></i>If You Are Mildly ill, Stay Home and Call a Health Care Provider<br>
 <br>
-        <i class="fas fa-check"style="color:blue"></i>Stay home and call your doctor for medical advice before vising a medical office. Older adults and people of any age with serious underlying medical conditions should call a health care provider as soon as symptoms start.<br>
+
+<i class="fas fa-check"style="color:blue"></i>Stay home and call your doctor for medical advice before vising a medical office. Older adults and people of any age with serious underlying medical conditions should call a health care provider as soon as symptoms start.<br>
 <i class="fas fa-check"style="color:blue"></i>Avoid public transportation, ridesharing and taxis.<br>
-        <i class="fas fa-check"style="color:bluen"></i>Separate yourself from other people in your home. As much as possible, stay in a specific room away from other people and pets. Use a separate bathroom, if available. Learn more about if you have pets.<br>
+<i class="fas fa-check"style="color:bluen"></i>Separate yourself from other people in your home. As much as possible, stay in a specific room away from other people and pets. Use a separate bathroom, if available. Learn more about if you have pets.<br>
 <i class="fas fa-check"style="color:blue"></i>Wear a cloth face covering if you are around other people (e.g., sharing a room or vehicle) and before you enter a health care provider’s office. See CDC guidelines.<br>
 <i class="fas fa-check"style="color:blue"></i>Continue to follow good hygiene practices. <br>
 <i class="fas fa-check"style="color:blue"></i>Avoid sharing personal household items, such as dishes, drinking glasses, cups, eating utensils, towels or bedding. Wash items thoroughly after using them with soap and water or put them in the dishwasher. <br>
@@ -126,7 +151,8 @@ Here’s how you can help:<br>
     </div>
     <br>
     <br>
-      <div class="card">
+
+    <div class="card">
       <h2 style="color: blue">If your symptoms are severe, call to get medical attention immediately.</h2>
       <br>
       <br>
@@ -143,7 +169,9 @@ Here’s how you can help:<br>
 <i class="fas fa-exclamation"style="color:red"></i>New confusion<br>
 <i class="fas fa-exclamation"style="color:red"></i>Inability to wake or stay awake<br>
 <i class="fas fa-exclamation"style="color:red"></i>Bluish lips or face<br>
-        This list is not all inclusive. Please consult your medical provider for any other symptoms that are severe or concerning.<br>
+ 
+
+This list is not all inclusive. Please consult your medical provider for any other symptoms that are severe or concerning.<br>
   </p>
     </div>
     <br>
@@ -156,7 +184,19 @@ Here’s how you can help:<br>
       <br>
       
       <!--div class="fakeimg" style="height:200px;">Image</div-->
-      Alternatives to EPA-registered disinfectants include: <br>
+    
+      
+<p style="color:grey">
+        
+
+
+
+Wear disposable gloves to clean and disinfect. <br>
+Prior to disinfecting, use detergent, or soap and water, to clean dirty surfaces. <br>
+Most common EPA-registered household disinfectants will work. Use disinfectants appropriate for the surface.  <br>
+Follow the manufacturer’s instructions for all cleaning and disinfectant products (e.g., concentration, application method and contact time, etc.). <br>
+Products with EPA-approved emerging viral pathogens claims are expected to be effective against COVID-19, based on data for harder-to-kill viruses. <br>
+Alternatives to EPA-registered disinfectants include: <br>
 Diluted household bleach: Mix 5 tablespoons (1/3 cup) bleach per gallon of water, or 4 teaspoons bleach per quart of water. Make sure you have proper ventilation and that the bleach is not expired. <br>
 Alcohol solutions: Ensure your solution has at least 70% alcohol.<br>
 <br>
@@ -164,22 +204,29 @@ Alcohol solutions: Ensure your solution has at least 70% alcohol.<br>
     </div>
 <br>
 <br>
-    
-      
-<p style="color:grey">
-  Wear disposable gloves to clean and disinfect. <br>
-Prior to disinfecting, use detergent, or soap and water, to clean dirty surfaces. <br>
-Most common EPA-registered household disinfectants will work. Use disinfectants appropriate for the surface.  <br>
-Follow the manufacturer’s instructions for all cleaning and disinfectant products (e.g., concentration, application method and contact time, etc.). <br>
-Products with EPA-approved emerging viral pathogens claims are expected to be effective against COVID-19, based on data for harder-to-kill viruses. <br>
-        
- <div class="card">
+
+
+<div class="card">
       <h2 style="color: blue">Precautions for People at Higher Risk</h2>
       <br>
       <br>
       
       <!--div class="fakeimg" style="height:200px;">Image</div-->
-   Stay home if possible.<br>
+    
+      
+<p style="color: grey">According to the CDC, early information shows that some people are at higher risk of getting very sick from COVID-19. This includes adults who are 65 and older, and people of any age who:<br>
+        <br>
+
+
+
+Have serious underlying medical conditions, particularly if not well controlled, such as heart, lung or liver disease; diabetes; moderate to severe asthma; severe obesity; and chronic kidney disease requiring dialysis. <br>
+Have a weakened immune system, including those undergoing cancer treatment, smoking or having other immunocompromised conditions.<br>
+ 
+
+If you are at higher risk for serious illness from COVID-19, it is extra important for you to take actions to avoid getting sick.<br>
+<br>
+
+Stay home if possible.<br>
 Wash your hands often.<br>
 Take everyday precautions to keep space between yourself and others (stay 6 feet away, which is about two arm lengths).<br>
 Keep away from people who are sick.<br>
@@ -190,48 +237,42 @@ Call your health care professional if you have concerns about COVID-19 and your 
 See additional healthy practices to protect yourself.<br>
 
 <br>
-    <br>
+<br>
 <b> </style>Older Adults and COVID-19: What You Can Do<br></b>
     <br>
 In addition to following the steps above, we recommend that you take these sensible precautions. <br>
 
 Create a personal support network of trusted individuals who can help set up your phone, computer or tablet and check in with you by phone or video calls, text or email to ensure your wellness. They can also help you with errands, groceries, online shopping and setting up telemedicine (i.e., medical appointments over the phone or by video). Share your important health and medical information with them and be sure they know how they can help you.<br>
 <br>
-      
-<p style="color: grey">According to the CDC, early information shows that some people are at higher risk of getting very sick from COVID-19. This includes adults who are 65 and older, and people of any age who:<br>
-        <br>
-
- Have serious underlying medical conditions, particularly if not well controlled, such as heart, lung or liver disease; diabetes; moderate to severe asthma; severe obesity; and chronic kidney disease requiring dialysis. <br>
-Have a weakened immune system, including those undergoing cancer treatment, smoking or having other immunocompromised conditions.<br>
-  Limit visits with family members, especially young children. While you may need family members to help run errands, keep about 6 feet away from them. Have visitors leave food or grocery items at the door rather than entering the home. Avoid inviting young children into your home.<br>
+Limit visits with family members, especially young children. While you may need family members to help run errands, keep about 6 feet away from them. Have visitors leave food or grocery items at the door rather than entering the home. Avoid inviting young children into your home.<br>
 <br>
 Have a 1-month supply of medications and a 2-week supply of food on hand.<br>
 <br>
 Along with prescription medications, have a 1-month supply of over-the-counter medications like cough suppressants and fever reducing drugs. Ask your physician or local pharmacy if prescriptions can be delivered. Be sure to have a thermometer on hand in case you need to check for a fever.<br>
 <br>
-A 2-week supply of food als
-  
-  local grocery stores to see if they offer online ordering and home delivery. Find out if there are local nonprofit services to assist with food or meal delivery.<br>
+A 2-week supply of food also includes water, household cleaning supplies, personal hygiene items, and medical supplies or equipment. Contact your local grocery stores to see if they offer online ordering and home delivery. Find out if there are local nonprofit services to assist with food or meal delivery.<br>
 <br>
 Have a plan for if you get sick. Make a list of medications and medical contacts, and know where to find health care documents like advance directives (a living will). Determine who will care for you at home should you become ill, and tell them what sort of care you would want if you became too ill and unable to speak for yourself. <br>
 <br>
 Cancel all non-essential, in-person doctor’s appointments. Use telemedicine services, which enables you to communicate with your doctor over video, phone or email, rather than face-to-face. <br>
 <br>
 Beware of scams. Unfortunately, scammers are taking advantage of people’s fears and the Federal Trade Commission has tips to help protect you and others. There currently are no vaccines, pills, lotions or other prescription or over-the-counter products available to treat or cure COVID-19 online or in stores. Beware of “viral videos” suggesting potentially dangerous or toxic home preventatives, such as drinking excessive amounts of water or mouthwash<br>
- 
-
-If you are at higher risk for serious illness from COVID-19, it is extra important for you to take actions to avoid getting sick.<br>
 <br>
-  <br>
-<b> </style>Older Adults and COVID-19: What You Can Do<br></b>
-    <br>
-In addition to following the steps above, we recommend that you take these sensible precautions. <br>
-
-Create a personal support network of trusted individuals who can help set up your phone, computer or tablet and check in with you by phone or video calls, text or email to ensure your wellness. They can also help you with errands, groceries, online shopping and setting up telemedicine (i.e., medical appointments over the phone or by video). Share your important health and medical information with them and be sure they know how they can help you.<br>
-<br>
-  <br>
   </p>
-  <p style="color: grey">
+    </div>
+<br>
+<br>
+
+
+<div class="card">
+      <h2 style="color: blue">Coping With Stress During COVID-19</h2>
+      <br>
+      <br>
+      
+      <!--div class="fakeimg" style="height:200px;">Image</div-->
+    
+      
+<p style="color: grey">
         
 
 
@@ -273,16 +314,7 @@ If you are religious or spiritual, follow practices at home that provide you wit
  <br>
 
 Helping Others:<br>
-    </div>
 <br>
-<br>
-
-
-<div class="card">
-      <h2 style="color: blue">Coping With Stress During COVID-19</h2>
-      <br>
-      <br>
-  <br>
 <i class="fas fa-check"style="color:blue"></i>Reach out to older adults or people with chronic health conditions and offer to help. For example, offer to pick up groceries, medications and other essential supplies. Check in with them regularly but practice social distancing by keeping at least six feet away when you deliver essential items and wearing a cloth face cover.<br>
 <i class="fas fa-check"style="color:blue"></i>Talk to your children and explain why this is happening and how long it might last. Use language that is normal and consistent with how you usually communicate. Be creative and think of fun activities that will occupy their time. Keep a schedule, set appropriate limits and maintain usual rules when possible.<br>
 <i class="fas fa-check"style="color:blue"></i>Take care of your pets, which can be an essential part of your support system. Like people, pets react to changes in their environment and routine, so their behaviors may change, as well. Keep track of their well-being and take care of their needs as best you can.<br>
@@ -291,7 +323,20 @@ Helping Others:<br>
     </div>
 <br>
 <br>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="assets/js/main.js"></script>
@@ -309,4 +354,3 @@ Helping Others:<br>
 
   </body>
 </html>
-      
