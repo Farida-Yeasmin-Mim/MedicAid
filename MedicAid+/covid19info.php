838f5a7c0eb14e6fb24eff8b4bc193f5aef76128
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-16 04:28:58 +0600

    Update covid19info.php

--- MedicAid+/covid19info.php
+++ MedicAid+/covid19info.php
@@ -1,1 +1,26 @@
 
+<doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <!-- Font Awesome CSS -->
+  
+   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    
+     
+
+    
+  </head>
+  <header>
+     
+   <?php include 'header.php';?>
+
+  </header>
+
+  <body>
