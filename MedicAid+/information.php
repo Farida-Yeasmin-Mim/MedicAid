f4ee09a379ed9ccec2e7ba1374669c772cdb1e60
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-16 04:45:07 +0600

    Update information.php

--- MedicAid+/information.php
+++ MedicAid+/information.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<DOCTYPE html>
 <html lang="en" dir="ltr">
 <html>
 <head>
@@ -20,39 +20,15 @@
     <!--a href="https://www.vectorstock.com/royalty-free-vector/young-man-gives-blood-medical-vector-31652142">Young man gives blood medical vector by vectorstock</a-->
     <!--a href='https://www.freepik.com/photos/background'>Background photo created by kbza - www.freepik.com</a-->
 </head>
+<header>
+     
+   <?php include 'header.php';?>
+
+  </header>
 <body>
 
 
-    <!--header-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <a class="navbar-brand" href="#">
-    <img src="Image/Main Logo.jpg" alt="Logo" style="width:153px"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Donation</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Ambulance</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-  <!--header end-->
-
+    
 
 
   <div class="card">
@@ -113,6 +89,9 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
+
 
 
 
