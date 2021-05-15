50a447a482107e4d602120933792db3deafec31b
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-16 01:49:31 +0600

    Add files via upload

---
+++ MedicAid+/Bangladesh_covid_info.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html>
+<head>
+  <meta charset="utf-8">
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="CSS/covid19.css">
+
+
+</head>
+
+<header>
+   <?php include 'header.php';?>
+</header>
+
+
+<body style="background-color:black;">
+
+
+
+  
+
+
+
+<div class="main_header">
+  <div class="row w-100 h-100">
+    <div class="col-lg-5 col-md-5 col-12 order-lg-1 order-2">
+      <div class="leftside w-100 h-100 d-flex justify-content-center align-items-center">
+        <img src="Image/corona5.png" width="550" height="400">
+    </div>
+  </div>
+
+    <div class="col-lg-7 col-md-7 col-12 order-lg-2 order-1">
+      <div class="rightside w-100 h-100 d-flex justify-content-center align-items-center">
+        <h1 style="color: white"> Bangladesh Covid-19 statistics  <span class="corona"> <img src="Image/covid.png"></span> </h1>
+        
+    </div>
+  </div>
+</div>
+</div>
+<br>
+<br>
+
+<br>
+<br>
+
+    
+
+
+
+
+
+<section class="corona_update container-fluid">
+  <div class="my-5">
+    <h3 class="text-center text-uppercase text-primary"> Bangladesh Covid-19 live updates</h3>
+  </div>
+  
+
+  <iframe src="https://covid-19.dataflowkit.com/assets/widget/c19-widget-dark.html" 
+frameborder="0" scrolling="no"
+width="100%" height="950">
+</iframe>
+<br>
+<br>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js" integrity="sha512-d8F1J2kyiRowBB/8/pAWsqUl0wSEOkG5KATkVV4slfblq9VRQ6MyDZVxWl2tWd+mPhuCbpTB4M7uU/x9FlgQ9Q==" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</section>
+
+
+            
+
+    
+
+
+
+
+
+</body>
+</html>
