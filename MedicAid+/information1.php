189c2decdbd644eff25b06f7a7643cb43f3cee08
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-20 04:09:02 +0600

    Update information1.php

--- MedicAid+/information1.php
+++ MedicAid+/information1.php
@@ -8,7 +8,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/information1.css">
-
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script async src="https://unpkg.com/typer-dot-js@0.1.0/typer.js"></script>
 
 </head>
 <header>
@@ -16,10 +17,9 @@
    <?php include 'header.php';?>
 
   </header>
-<body>
+  <br>
+<body style="background-color: #404040">
 
-
-  
 
 
 
@@ -30,39 +30,66 @@
 
 
   <div class="header">
-  <h2>Blood Donation Process</h2>
+    
+    <h1>Typer.js is
+  <span class="typer" id="main" data-words="Blood ,Donation,Process" data-delay="90" data-deleteDelay="999" data-colors="#b74033"></span>
+  <span class="cursor" data-owner="main"></span>
+</h1>
+
 </div>
 
 <div class="row">
   <div class="leftcolumn">
     <div class="card">
-      <h2>Donation Process Overview</h2>
-      <h5>Registration and Health History</h5> 
+
+      <h2 style="color: blue;">Donation Process Overview</h2>
+      
+      <i class="fas fa-user-plus"></i><h5>Registration and Health History</h5> 
+      <div class="img-container"> <!-- Block parent element -->
+    <img src="Image/5266696.jpg" width="700"height="500">
+     <style>
+      .img-container {
+        text-align: center;
+      }
+  </style>
       <!--div class="fakeimg" style="height:200px;">Image</div-->
-      <img src="Image/5266696.jpg" height="900">
-      <p style="background:    #F5F5DC;"> 1. We’ll sign you in and you will go over Blood Donation eligibility.<br>
-        2. You’ll read some information about donating blood.<br>
-        3.You’ll answer a few questions about your health history and places you’ve traveled, during a private and confidential interview.<br>
-        4.You’ll tell us about any prescription and/or over the counter medications that may be in your system.<br>
-        5.We’ll check your temperature, pulse, blood pressure and hemoglobin level.</p>
+      
+      <p style="background:    #F5F5DC;"> <i class="fas fa-hand-point-right"></i> 1. We’ll sign you in and you will go over Blood Donation eligibility.<br>
+        <i class="fas fa-hand-point-right"></i>2. You’ll read some information about donating blood.<br>
+        <i class="fas fa-hand-point-right"></i>3.You’ll answer a few questions about your health history and places you’ve traveled, during a private and confidential interview.<br>
+        <i class="fas fa-hand-point-right"></i>4.You’ll tell us about any prescription and/or over the counter medications that may be in your system.<br>
+       <i class="fas fa-hand-point-right"></i> 5.We’ll check your temperature, pulse, blood pressure and hemoglobin level.</p>
     </div>
 
 
     <div class="card">
-      <h2>Donation and Recovery</h2>
+      </i><h2 style="color: blue;">Donation and Recovery</h2>
+
+      <div class="img-container"> <!-- Block parent element -->
+     <img src="Image/information1.jpg"  width ="700" height="500">
+     <style>
+      .img-container {
+        text-align: center;
+      }
+  </style>
+    </div>
       
       <!--div class="fakeimg" style="height:200px;">Image</div-->
-      <img src="Image/information1.jpg" height="900">
-      <p>If you’re donating whole blood, we’ll cleanse an area on your arm and insert a brand new sterile needle for the blood draw. (This feels like a quick pinch and is over in seconds.)<br>
-Other types of donations, such as platelets, are made using an apheresis machine which will be connected to both arms.<br>
-A whole blood donation takes about 8-10 minutes, during which you’ll be seated comfortably or lying down.<br>
-When approximately a pint of whole blood has been collected, the donation is complete and a staff person will place a bandage on your arm.<br>
-For platelets, the apheresis machine will collect a small amount of blood, remove the platelets, and return the rest of the blood through your other arm; this cycle will be repeated several times over about 2 hours.<br>
+      
+      <p><i class="fas fa-hand-point-right"></i>If you’re donating whole blood, we’ll cleanse an area on your arm and insert a brand new sterile needle for the blood draw. (This feels like a quick pinch and is over in seconds.)<br>
+<i class="fas fa-hand-point-right"></i>Other types of donations, such as platelets, are made using an apheresis machine which will be connected to both arms.<br>
+<i class="fas fa-hand-point-right"></i>A whole blood donation takes about 8-10 minutes, during which you’ll be seated comfortably or lying down.<br>
+<i class="fas fa-hand-point-right"></i>When approximately a pint of whole blood has been collected, the donation is complete and a staff person will place a bandage on your arm.<br>
+<i class="fas fa-hand-point-right"></i>For platelets, the apheresis machine will collect a small amount of blood, remove the platelets, and return the rest of the blood through your other arm; this cycle will be repeated several times over about 2 hours.<br>
 <br>
-After donating blood, you’ll have a snack and something to drink in the refreshment area.<br>
-You’ll leave after 10-15 minutes and continue your normal routine.<br>
-Enjoy the feeling of accomplishment knowing you are helping to save lives.<br>
-Take a selfie, or simply share your good deed with friends. It may inspire them to become blood donors.<br>
+<i class="far fa-smile-beam"></i>
+
+
+<br>
+<i class="fas fa-hand-point-right"></i>After donating blood, you’ll have a snack and something to drink in the refreshment area.<br>
+<i class="fas fa-hand-point-right"></i>You’ll leave after 10-15 minutes and continue your normal routine.<br>
+<i class="fas fa-hand-point-right"></i>Enjoy the feeling of accomplishment knowing you are helping to save lives.<br>
+<i class="fas fa-hand-point-right"></i>Take a selfie, or simply share your good deed with friends. It may inspire them to become blood donors.<br>
   </p>
     </div>
 
@@ -77,6 +104,9 @@ Take a selfie, or simply share your good deed with friends. It may inspire them 
 
 </body>
 </html>
+
+
+
 
 
 
