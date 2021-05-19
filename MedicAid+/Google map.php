981cc1f320f5d8e22b4a95b52465dce0919a0e20
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-19 17:27:30 +0600

    Update Google map.php

--- MedicAid+/Google map.php
+++ MedicAid+/Google map.php
@@ -1,69 +1,28 @@
-<!--DOCTYPE html>
+	<DOCTYPE html>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<style>
-		
+  <title></title>
+  <style>
+    
 
-		*{
+    *{
 
-			margin: 0; padding: 0; box-sizing: border-box;
-		}
-	</style>
-	
+      margin: 0; padding: 0; box-sizing: border-box;
+    }
+  </style>
+  
 </head>
+<header>
+     
+   <?php include 'header.php';?>
+
+  </header>
 <body>
  <section>
- 	
- 	    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=11LMaX8AWCP2G-bhaWDCEUhnN3x1hSc_W" width="100%" height="740" ></iframe>
+  
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=11LMaX8AWCP2G-bhaWDCEUhnN3x1hSc_W" width="100%" height="740" ></iframe>
  </section>
 </body>
-<--/html>
-
-
-
-
-
-
-		
-	
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="googlemap.js"></script>
-	<style type="text/css">
-		
-		.container{
-			height: 700px;
-		}
-		#map{
-			width: 100%;
-			height:100%;
-			border: 1px solid blue;
-		}
-	</style>
-</head>
-<body style="background-color:gray">
-	<header>
-		<?php include 'header.php';?>
-	</header>
-	<br>
-	<br>
-	<div class="container">
-		
-		<div id="map"></div>
-	</div>
-
-</body>
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIwNNFGkb2kP2jLBMGNNIYJ3rivzI6KSY&callback=loadMap">
-</script>
-
 </html>
-
-
 
