9e33b0dc11fb3be1991e387d9d96dc582ee8f787
zarintasnimturna <80968166+zarintasnimturna@users.noreply.github.com>
2021-05-20 05:22:34 +0600

    Update information3.php

--- MedicAid+/information3.php
+++ MedicAid+/information3.php
@@ -8,6 +8,9 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/information3.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script async src="https://unpkg.com/typer-dot-js@0.1.0/typer.js"></script>
+
 </head>
 <header>
      
@@ -15,83 +18,107 @@
 
   </header>
 
-<body style="background-color:#F5F5DC #FFF0F5;">
+<body style="background-color:#262626;">
+  <br>
   
   <div class="header">
-  <h2>Common Concerns</h2>
-  <p></p>
+  
+  <span class="typer" id="main" data-words="COMMON, CONCERNS." data-delay="90" data-deleteDelay="999" data-colors="#b74033"></span>
+  <span class="cursor" data-owner="main"></span>
+</h1>
+
+</div>
   
 </div>
 <div class="row">
   <div class="leftcolumn">
     <div class="card">
-      <h2>I Don't Like Needles</h2>
+      <h2 style="color: blue;">I Don't Like Needles <i class="fas fa-syringe"></i></h2>
       <h5>You’re not alone: fear of needles certainly prevents some people from donating blood. But if you’re willing to spend a few minutes facing your fears (donating really can be that fast) you may find that the satisfaction you feel makes it worth it.</h5> <br>
+      <div class="img-container"> <!-- Block parent element -->
+    <img src="Image/information31.jpg" width="700" height="600">
+     <style>
+      .img-container {
+        text-align: center;
+      }
+  </style>
+      
       <br>
       <br>
       <br>
       <!--div class="fakeimg" style="height:200px;">Image</div-->
-      <img src="Image/information31.jpg" height="900">
-      <p style="background:    #F5F5DC;"> Try the Helpful Tips Below:<br>
+      
+      <b><p style="color: blue;"> Try the Helpful Tips Below:<br></p>
+        <p>
         <br>
-        1.Take Pride<br>
+        <i class="far fa-hand-point-right"></i>Take Pride<br>
 Try to focus on the good you are doing - your blood donation may help save as many as 3 lives. <br>
-2.Be Prepared<br>
+<i class="far fa-hand-point-right"></i>Be Prepared<br>
 Before your appointment, read up on the donation process so you know what to expect at every step.<br>
-Relax<br>
+<i class="far fa-hand-point-right"></i>Relax<br>
 Listen to music, read a book, talk to our staff, or simply close your eyes and rest for a few minutes.<br>
 </p>
 <br>
 <br>
 <br>
-<p style="background: #F5F5DC">I'm Not Sure I'm Eligible:<br>
-Approximately 38% of Americans are eligible to donate blood at any given time, so there’s a very good chance you meet the criteria.For most donations, these are the basic requirements:  1.Good Health: <br>
+<p style="color: blue;">I'm Not Sure I'm Eligible:<br></p>
+<p>
+Approximately 38% of Americans are eligible to donate blood at any given time, so there’s a very good chance you meet the criteria.For most donations, these are the basic requirements: Good Health: <br>
 You generally feel well, even if you're being treated for a chronic condition. <br>
-Age 17 <br>
+<i class="far fa-hand-point-right"></i>Age 17 <br>
 16 year olds may donate with parental permission in certain states if allowed by state law.<br>
-110 lbs<br>
-Additional weight requirements apply for donors age 18 or younger
+<i class="far fa-hand-point-right"></i>110 lbs<br>
+Additional weight requirements apply for donors age 18 or younger.
 </p>
 <br>
 <br>
 <br>
-<p style="background:    #F5F5DC">Is it Safe to Donate?: <br>
-Most donors feel fine after donating blood, but if you do feel faint or fatigued, simply lie down until you feel better.<br>
+<p style="color: blue;">Is it Safe to Donate?: <br></p>
+<p>
+<i class="far fa-hand-point-right"></i>Most donors feel fine after donating blood, but if you do feel faint or fatigued, simply lie down until you feel better.<br>
 <br>
-It might help you to think about this: you have about 10 pints of blood in your body. Roughly 1 pint is given during a donation.<br>
+<i class="far fa-hand-point-right"></i>It might help you to think about this: you have about 10 pints of blood in your body. Roughly 1 pint is given during a donation.<br>
 <br>
-Your body can replenish that loss in a short time (a matter of hours for some components, a few weeks for others).<br>
+<i class="far fa-hand-point-right"></i>Your body can replenish that loss in a short time (a matter of hours for some components, a few weeks for others).<br>
 <br>
 </p>
 <br>
 <br>
 <br>
-<p style="background:    #F5F5DC">I'm Afraid I'll Faint:<br>
-During an AB Elite donation, you give plasma, a part of your blood used to treat patients in emergency situations.<br>
-AB plasma can be given to anyone regardless of their blood type.<br>
+<p style="color: blue;">I'm Afraid I'll Faint:<br></p>
+<p>
+<i class="far fa-hand-point-right"></i>During an AB Elite donation, you give plasma, a part of your blood used to treat patients in emergency situations.<br>
+<i class="far fa-hand-point-right"></i>AB plasma can be given to anyone regardless of their blood type.<br>
 </p>
 <br>
 <br>
     </div>
     <div class="card">
-      <h2>I Don't Have the Right Kind of Blood</h2>
+      <h2 style="color: blue;">I Don't Have the Right Kind of Blood</h2>
+      <div class="img-container"> <!-- Block parent element -->
+    <img src="Image/information32.jpg" width="700"height="600">
+     <style>
+      .img-container {
+        text-align: center;
+      }
+  </style>
       
-      <!--div class="fakeimg" style="height:200px;">Image</div-->
-      <img src="Image/information32.jpg" height="900">
-      <p style="background: #F5F5DC">Perhaps you’ve heard an appeal for donors that says we’re running low on certain blood types. That doesn’t mean other types aren’t valuable: there’s always a need and purpose for every type of blood. Here’s what we’re looking for today:<br>
+      <br>
+      <p>Perhaps you’ve heard an appeal for donors that says we’re running low on certain blood types. That doesn’t mean other types aren’t valuable: there’s always a need and purpose for every type of blood. Here’s what we’re looking for today:<br>
         <br>
-        All Types<br>
+        <i class="far fa-hand-point-right"></i>All Types<br>
         <br>
 Whether you’re A, B, AB or O – or unknown – all types of blood are valuable and can help save lives.<br>
 <br>
-All Components<br>
+<i class="far fa-hand-point-right"></i>All Components<br>
 
 You can donate whole blood, or certain components such as red cells, platelets or plasma.<br>
 <br>
 
-Volunteer Donors<br>
-<br>#fefbd8
+<i class="far fa-hand-point-right"></i>Volunteer Donors<br>
+<br>
 The most important thing is that you are willing to make this generous, life-saving donation.<br>
+</b>
 
   </p>
     </div>
@@ -102,4 +129,5 @@ The most important thing is that you are willing to make this generous, life-sav
 </div>
 </body>
 </html>
+
 
