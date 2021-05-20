48e15d04afa12c55c6e9a809d0eab7828b3f9284
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-20 12:34:13 +0600

    Blood Page Updated

--- MedicAid+/blood.php
+++ MedicAid+/blood.php
@@ -10,6 +10,14 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+  body {
+  background-image: url('Image/bloodpb.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  }
+  </style>
   </head>
   <body>
     <header>
@@ -34,35 +42,38 @@
       </div>
 
       <section>
+        <div class="container-fluid">
+
+        <hr><br><br>
         <div class="blog">
           <div class="container">
             <div class="row mb-5">
               <div class="col-md-6 col-lg-6 col-sm-12">
                 <div class="card">
                   <div class="card-img">
-                    <img src="Image/bloodpage.jpg" class="img-fluid">
+                    <img src="Image/creatacc1.jpg" class="img-fluid">
                   </div>
 
                   <div class="card-body">
-                    <h4 class="card-title">Blood Donation</h4>
+                    <h4 class="card-title">Sign Up As A Donor</h4>
                     <p class="card-text">
 
                       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                   </div>
                   <div class="card-footer">
-                    <a href="" class="card-link">Read more</a>
+                    <a href="" class="card-link">Sign Up For Free Now!</a>
                   </div>
                 </div>
               </div>
               <div class="col-md-6 col-lg-6 col-sm-12">
                 <div class="card">
                   <div class="card-img">
-                    <img src="Image/bloodpage.jpg" class="img-fluid">
+                    <img src="Image/bloodpi.jpg" class="img-fluid">
                   </div>
 
                   <div class="card-body">
-                    <h4 class="card-title">Ambulance</h4>
+                    <h4 class="card-title">Information</h4>
                     <p class="card-text">
 
                       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -88,10 +99,10 @@
             <br>
             <br>
 
-            <h1 class="text-dark">Donor Details</h1>
+            <h1 class="text-dark">Are you  eligible to donate blood?</h1>
               <p class="text-dark">Donation is a practice when people donate to others to help them with their critical conditions. View their details and you can also block user if they are not useful. </p>
 
-              <button class="btn bg-primary text-white" onclick="window.location ='report_user.php?id=<?php echo $dbid ?>'">View Donor Details</button>
+              <button class="btn bg-primary text-white" onclick="window.location ='report_user.php?id=<?php echo $dbid ?>'">Eligibility Criteria</button>
         </div>
           <div class="col-lg-6 col-md-6 col-12">
             <img src="Image/bloodreq.jpg" class="img-fluid">
