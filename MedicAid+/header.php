edce15e5b73a6e55531489b01b0f5a734e9521cc
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-21 23:45:48 +0600

    Added Some Menu To Header

--- MedicAid+/header.php
+++ MedicAid+/header.php
@@ -24,22 +24,32 @@
 
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Donation</a>
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="blood.php">Donation</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Volunteer</a>
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="volunteer.php">Volunteer</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" style="font-size:23px; font-weight:bold" href="#">Ambulance</a>
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="ambulance.php">Ambulance</a>
       </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="appointment.php">Appointment</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" style="font-size:23px; font-weight:bold" href="feedback.php">Feedback</a>
+      </li>
+
     </ul>
 
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    <form id="searchform" method="POST" action="index.php" class="form-inline my-2 my-lg-0">
+      <input name="srch" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button name="search" class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     </form>
 
   </div>
