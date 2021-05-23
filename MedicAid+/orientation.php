171c2b9a306d11fab44c29ca6f6a1f759c5d0c70
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-23 16:40:17 +0600

    Create orientation.php

---
+++ MedicAid+/orientation.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Orientation</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/volunteer.css">
+  </head>
+  <body>
+
+
+<div class="row justify-content-center">
+<iframe width="950" height="500" src="https://www.youtube.com/embed/5bSMlAuQqsg" allowfullscreen></iframe>
+</div>
+
+
+
+</body>
+</html>
