cc569a94c34c9dfe14c40e6ba89c8bea842ec116
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-23 18:51:11 +0600

    Create disaster.php

---
+++ MedicAid+/disaster.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Disaster</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/index.css">
+
+  </head>
+  <body>
+
+
+  <header>
+      <?php include 'header.php';?>
+  </header>
+
+
+
+  <div class="wrap-about text-center ">
+    <div class="heading-section pr-md-5 pt-md-5 ">
+    <h1 class="mb-4 text-dark">When disasters happen, Red Cross volunteers spring into action</h1>
+
+    <div class="row justify-content-center">
+    <iframe width="500" height="300" src="https://www.youtube.com/embed/-yamNDUbsn0" allowfullscreen></iframe>
+    </div>
+<br>
+      <p style="color: grey">When disaster strikes, people’s lives change in an instant. Often, they lose everything they hold dear —
+        their home, family photos, clothes, furniture — everything. When the worst happens, American Red Cross volunteers are there to help.<br>
+<br>
+
+
+<h5 style="color: black">1881 MICHIGAN FOREST FIRE</h5>
+<p style="color: grey">Months after Clara Barton founded the American Red Cross, the organization launched its first disaster response —
+  donating money, clothes and furniture to victims of the devastating forest fires in Michigan. The fire claimed almost 300 lives and
+  left thousands homeless. Red Cross chapters collected food and supplies, which were shipped to Michigan to help assist the 14,000 people
+  in need.</p>
+
+<br>
+
+<h5 style="color: black">1889 JOHNSTOWN FLOOD In 1889</h5>
+<p style="color: grey">a dam collapsed near Johnstown, Pennsylvania, sending millions of gallons of water toward the town of 30,000 people. More than 2,000 people died, hundreds were missing and tens of thousands were left homeless. To help, Clara Barton and a team of doctors, nurses and relief workers set up hospital tents, feeding stations and our first sheltering operation with “Red Cross hotels” to house the people whose homes were destroyed. Clara Barton and her team of 50 volunteers remained in Johnstown for months, helping the survivors get back on their feet.</p>
+<br>
+
+<h5 style="color: black">1889 JOHNSTOWN FLOOD In 1889</h5>
+<p style="color: grey">SC Before hurricanes were named, the Red Cross launched its first major hurricane response when a powerful storm struck the Sea Islands in South Carolina in August 1893, claiming the lives of thousands of residents. As bad as the damage was, starvation was a chief concern. Red Cross volunteers, led by President Clara Barton, helped feed thousands of people until the next year’s spring crops were harvested. She set up a warehouse to stock clothing and food to support those in need. For nearly a year, the Red Cross stood with survivors who lost everything, helping them rebuild their homes and livelihoods
+  by providing seeds for new crops and creating local sewing groups for people to create and sell clothes.<p>
+<br>
+
+<h5 style="color: black">1927 MISSISSIPPI RIVER FLOOD</h5>
+ <p style="color: grey">The Red Cross was called into action along the Mississippi River during the Great Flood of 1927,
+   which covered about 16 million acres in Louisiana, Mississippi, Arkansas, Tennessee, Missouri, Illinois and Kentucky.
+   The Red Cross helped with transportation, sheltering, feeding and clothing, as well as furnishing nursing and medical care.<p>
+
+<br>
+
+<h5 style="color: black">1969 HURRICANE CAMILLE</h5>
+ <p style="color: grey">On August 17, 1969, Hurricane Camille made landfall on the Gulf Coast of Louisiana, Mississippi and Alabama as a deadly Category 5 storm, causing extensive damage and unprecedented flash flooding. At the time, it was the most damaging storm on record in the U.S. During the height of the response, the Red Cross fed and sheltered nearly 260,000 disaster victims and relief workers. In coordination with federal and state authorities, the Red Cross assisted families with their long-term recovery and returning to their daily lives.</p>
+    </div>
+
+  </div>
+
+
+
+</body>
+</html>
