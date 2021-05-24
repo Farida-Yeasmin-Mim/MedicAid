ffb569bd485bd522f6caf024264691f3a837167e
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-24 17:48:01 +0600

    Update about.php

--- MedicAid+/about.php
+++ MedicAid+/about.php
@@ -10,7 +10,7 @@ include 'connection.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <title>About</title>
+    <title>About MedicAid</title>
 
     <!-- about.css-->
     <link rel="stylesheet" href="CSS/about.css">
