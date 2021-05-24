5ea3502380ff970f6f2323174336d08d78f1c3f3
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-24 17:19:13 +0600

    Update about.php

--- MedicAid+/about.php
+++ MedicAid+/about.php
@@ -1,14 +1,48 @@
+<?php
+include 'connection.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>About Us</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="CSS/volunteer.css">
+    <meta name="viewport" content="width=device-wiidth,initial-scale=1">
+    <title>About</title>
+
+    <!-- about.css-->
+    <link rel="stylesheet" href="CSS/about.css">
+
   </head>
   <body>
+    <header>
+      <?php include 'header.php';?>
+    </header>
+
+    <div class="aboutBground">
+
+    <div id='AppendHere'></div>
+
+    <!-- About Cooking realm section-->
+    <div class="About_Cooking_realm">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-md-3">
+            <div class="image__background">
+              <img src="Image/image_1.jpg" class="image__responsive">
+            </div>
+          </div>
+
+        <div class="col-md-8">
+          <h4>ABOUT MEDICAID</h4>
+           <p> Cooking is a process that often becomes a great pastime for many people. Here you can find a vast number of recipes, learn diffent cooking skills and upload recipes along with photos of the dish. There are many exciting events and contests for the users. Moreover, a user can buy membership and get many benefits than the regular user. There are also have some amazing features. </p>
+          <a href="index.php" class="botton">See More</a>
+        </div>
+
+        </div>
+
+      </div>
+    </div>
+
+  </body>
+</html>
