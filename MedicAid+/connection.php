1e086d12599c111cc815f42fb534c09d351965b3
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-25 23:15:37 +0600

    Update connection.php

--- MedicAid+/connection.php
+++ MedicAid+/connection.php
@@ -5,7 +5,6 @@ $password = "";
 $dbname = "medic_aid_plus";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-
 if (!$conn) {
 die("Connection failed: " . mysqli_connect_error());
 }
