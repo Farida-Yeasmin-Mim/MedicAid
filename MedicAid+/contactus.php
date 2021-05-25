6663386c605fe3b428be2ce4f1d272c41ef29542
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-25 23:18:09 +0600

    Update contactus.php

--- MedicAid+/contactus.php
+++ MedicAid+/contactus.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <title>Contact Us</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
     <!-- contactus.css-->
     <link rel="stylesheet" href="CSS/contactus.css">
   </head>
