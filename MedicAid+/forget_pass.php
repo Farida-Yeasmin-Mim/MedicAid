f89b9316972c8fda63e1796810b70fd5ca188931
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-25 23:28:09 +0600

    Update forget_pass.php

--- MedicAid+/forget_pass.php
+++ MedicAid+/forget_pass.php
@@ -48,7 +48,7 @@ $dbid =  $_GET['id'];
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Donor Login</title>
+    <title>Forget Password</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
