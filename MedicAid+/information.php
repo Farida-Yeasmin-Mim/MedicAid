cd243ef98eb3f160161ba6dba9eda96fd94f8063
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-25 23:28:26 +0600

    Update information.php

--- MedicAid+/information.php
+++ MedicAid+/information.php
@@ -3,32 +3,32 @@
 <html>
 <head>
   <meta charset="utf-8">
-    <title></title>
+    <title>Information</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  
+
     <link rel="stylesheet" href="information.css">
 
     <!--a href="https://www.vectorstock.com/royalty-free-vector/young-man-gives-blood-medical-vector-31652142">Young man gives blood medical vector by vectorstock</a-->
     <!--a href='https://www.freepik.com/photos/background'>Background photo created by kbza - www.freepik.com</a-->
 </head>
 <header>
-     
+
    <?php include 'header.php';?>
 
   </header>
 <body>
 
 
-    
+
 
 
   <div class="card">
@@ -37,12 +37,12 @@
     <br>
     <br>
     <br>
-    
+
 
   <div class="container">
     <div class="row">
 
-     
+
       <div class="col-sm-4 py-3 py-sm-0">
         <div class="card box-shadow">
           <img src="Image/information4.jpg" class="card-img-top" alt="...">
@@ -80,19 +80,12 @@
   <br>
 
 
-  
 
- 
+
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-
-
-
-
-
