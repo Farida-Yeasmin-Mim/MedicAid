60ab24abbd20a6993b39d92b48b11f503246799f
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-25 23:15:31 +0600

    Update edit_ambulance_profile.php

--- MedicAid+/edit_ambulance_profile.php
+++ MedicAid+/edit_ambulance_profile.php
@@ -113,7 +113,7 @@ $result = mysqli_query($conn, "SELECT * FROM ambulance WHERE ambulance_id='$dbid
 <!DOCTYPE html>
 <html>
 <head>
-<title>Ambulance Profile Edit</title>
+<title>Edit Ambulance Profile</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
