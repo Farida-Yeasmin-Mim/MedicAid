31bdba1d5c4cceccda2e8ab36c15cb064ca1573e
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-25 23:27:59 +0600

    Update Google map.php

--- MedicAid+/Google map.php
+++ MedicAid+/Google map.php
@@ -2,27 +2,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <title>Google Map</title>
   <style>
-    
+
 
     *{
 
       margin: 0; padding: 0; box-sizing: border-box;
     }
   </style>
-  
+
 </head>
 <header>
-     
+
    <?php include 'header.php';?>
 
   </header>
 <body>
  <section>
-  
+
       <iframe src="https://www.google.com/maps/d/u/0/embed?mid=11LMaX8AWCP2G-bhaWDCEUhnN3x1hSc_W" width="100%" height="740" ></iframe>
  </section>
 </body>
 </html>
-
