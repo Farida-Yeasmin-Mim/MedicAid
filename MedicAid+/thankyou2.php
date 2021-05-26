d5709ee5be5cbbe67494ff0fab0499ed3a567b42
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-26 20:46:22 +0600

    Update thankyou2.php

--- MedicAid+/thankyou2.php
+++ MedicAid+/thankyou2.php
@@ -8,7 +8,7 @@ include 'connection.php';
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Covid19</title>
+    <title>Registration Completed</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
