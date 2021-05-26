3cb48599743c7f377059ff363909a5bed9c37a37
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-26 21:42:36 +0600

    Ambulance Form Page Connected To Database
    Works Successfully!

--- MedicAid+/ambulance_form.php
+++ MedicAid+/ambulance_form.php
@@ -1,95 +1,45 @@
 <?php
 include 'connection.php';
 
-if (!empty($_GET['id'])){
-   $dbid =  $_GET['id'];
-   $sql = "SELECT full_name, email, contact_number, gender, age, blood_group, division, address FROM donor WHERE donor_id=$dbid";
-   $result = mysqli_query($conn,$sql);
-   $row = mysqli_fetch_assoc($result);
-   $name= $row["full_name"] ;
-   $email= $row["email"] ;
-   $contact= $row["contact_number"] ;
-   $gender= $row["gender"] ;
-   $age= $row["age"] ;
-   $blood_group= $row["blood_group"] ;
-   $division= $row["division"] ;
-   $address= $row["address"] ;
-
-   $sql="INSERT INTO plasma(full_name, email, contact_number, gender, age, blood_group, division, address)
-  VALUES ('$name','$email','$contact','$gender','$age','$blood_group','$division','$address')";
-  mysqli_query($conn, $sql);
-
-  $sql = "UPDATE donor
-  SET plasma_status='Active'
-  WHERE donor_id='$dbid' ";
-  // execute query
-  mysqli_query($conn, $sql);
-
-  $sql = "SELECT plasma_id FROM plasma WHERE contact_number='$contact' ";
-  $result = mysqli_query($conn,$sql);
-  $row = mysqli_fetch_assoc($result);
-  $dbp= $row["plasma_id"] ;
-
-  header("Location: thankyou2.php?id=$dbid&p=$dbp");
-
-}
-
 
 if(isset($_POST['signup'])){
 
 $dbfname = mysqli_real_escape_string($conn, $_POST['fullname']);
-$dbemail = mysqli_real_escape_string($conn, $_POST['email']);
 $dbcontact = mysqli_real_escape_string($conn, $_POST['contact']);
-$dbgender = mysqli_real_escape_string($conn, $_POST['gender']);
-$dbage = mysqli_real_escape_string($conn, $_POST['age']);
-$dbbgroup = mysqli_real_escape_string($conn, $_POST['bgroup']);
 $dbdivision = mysqli_real_escape_string($conn, $_POST['division']);
-$dbaddress = mysqli_real_escape_string($conn, $_POST['address']);
+$dbaddress = mysqli_real_escape_string($conn, $_POST['vehicle_no']);
 
 
-$sql = "SELECT email,contact_number FROM plasma";
+$sql = "SELECT contact_number FROM ambulance";
 $result = mysqli_query($conn,$sql);
 
 if (mysqli_num_rows($result) > 0) {
 // output data of each row
 while($row = mysqli_fetch_assoc($result)) {
-$dbbemail= $row["email"];
 $dbbcontact= $row["contact_number"];
 // Check if the username they entered was correct
-if ($dbbemail == $dbemail) {
-  echo "<script>
-alert('Same User Name Exist! Try Another User Name!');
-window.location.href='plasma_form.php';
-</script>";
-  exit();
-
-  }
-  else if($dbbcontact == $dbcontact)
+  if($dbbcontact == $dbcontact)
   {
     echo "<script>
   alert('Same Contact Number Exist! Try Another Contact Number!');
-  window.location.href='plasma_form.php';
+  window.location.href='ambulance_form.php';
   </script>";
     exit();
 
   }
-
-
-
-
 }
 }
- $sql="INSERT INTO plasma(full_name, email, contact_number, gender, age, blood_group, division, address)
-VALUES ('$dbfname','$dbemail','$dbcontact','$dbgender','$dbage','$dbbgroup','$dbdivision','$dbaddress')";
+ $sql="INSERT INTO ambulance(owner_name, contact_number, division, ambulance_number,status)
+VALUES ('$dbfname','$dbcontact','$dbdivision','$dbaddress','Active')";
 mysqli_query($conn, $sql);
 
-$sql = "SELECT plasma_id
-FROM plasma
-WHERE email='$dbemail'";
+$sql = "SELECT ambulance_id
+FROM ambulance
+WHERE contact_number='$dbcontact'";
 $result = mysqli_query($conn,$sql);
 $row = mysqli_fetch_assoc($result);
-$dbid= $row["plasma_id"];
-header("Location: thankyou.php?id=$dbid");
+$dbid= $row["ambulance_id"];
+header("Location: thankyou_ambulance.php?id=$dbid");
 
 }
 
@@ -111,12 +61,18 @@ header("Location: thankyou.php?id=$dbid");
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
    <link rel="stylesheet" href="CSS/signup.css">
    <body>
+
+     <header>
+       <?php include 'header.php';?>
+
+                 </header>
      <div class="card bg-dark text-white">
          <img class="card-img" src="Image/ambulancesigncover.jpg" alt="Card image">
        <div class="card-img-overlay">
      <article class="card-body mx-auto" style="max-width: 450px;">
 
-
+<br>
+<br>
 <div class="card bg-light">
 <article class="card-body mx-auto" style="max-width: 400px;">
   <div class="container-fluid">
@@ -160,17 +116,12 @@ header("Location: thankyou.php?id=$dbid");
   </div>
       <input name="vehicle_no" class="form-control" placeholder="Vehicle Number" type="text" required="">
   </div>
-    <div class="form-group input-group">
-    	<div class="input-group-prepend">
-		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
-		</div>
-        <input name="password"class="form-control" placeholder="Create password" type="password" required=" ">
-    </div>
+
 
     <div class="form-group">
         <button type="submit" name="signup" form="signform" class="btn btn-primary btn-block"> Register Now  </button>
     </div>
-    
+
 </form>
 </article>
 </div>
