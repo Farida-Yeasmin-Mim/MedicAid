9bd1be6fd324255f8313e127079eba273436b0d4
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-26 20:43:46 +0600

    Update admin_profile.php

--- MedicAid+/admin_profile.php
+++ MedicAid+/admin_profile.php
@@ -19,7 +19,7 @@ $dbname= $row["name"];
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Admin Profile</title>
+    <title>Profile | Admin</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
