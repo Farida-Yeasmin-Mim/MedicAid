653d74e143f7f82b3869e447448cf4812ed3a902
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-26 17:56:47 +0600

    Create expert_volunteer.php

---
+++ MedicAid+/expert_volunteer.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Expert Volunteer</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="CSS/expert_volunteer.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+<body>
+
+
+
+
+<section class="ftco-section">
+ <div class="container">
+  <div class="row justify-content-center pb-5 mb-3">
+    <div class="col-md-7 heading-section text-center ftco-animate">
+     <span class="subheading">Volunteer</span>
+     <h2>Our Expert Volunteer</h2>
+   </div>
+ </div>
+
+
+ <div class="row">
+    <div class="member">
+       <img src="Image/image_1.jpg">
+       <div class="inner">
+         <div class="info">
+           <h4 class="text-white">Farida Yeasmin Mim</h4>
+           <h5><p>Developer</p></h5>
+         </div>
+       </div>
+  </div>
+
+
+<div class="col-md-6 col-lg-3">
+  <div class="member">
+   <div class="img" style="background-image: url(images/team-2.jpg);"></div>
+   <div class="text text-2">
+    <h3>Cedrick Brown</h3>
+    <span>Volunteer</span>
+  </div>
+</div>
+</div>
+<div class="col-md-6 col-lg-3">
+  <div class="member">
+   <div class="img" style="background-image: url(images/team-3.jpg);"></div>
+   <div class="text text-3">
+    <h3>John Wick</h3>
+    <span>Volunteer</span>
+  </div>
+</div>
+</div>
+<div class="col-md-6 col-lg-3">
+  <div class="member">
+   <div class="img" style="background-image: url(images/team-4.jpg);"></div>
+   <div class="text text-4">
+    <h3>Max Love</h3>
+    <span>Volunteer</span>
+  </div>
+</div>
+</div>
+
+<div class="col-md-6 col-lg-3">
+  <div class="member">
+   <div class="img" style="background-image: url(images/team-5.jpg);"></div>
+   <div class="text text-4">
+    <h3>Alex Martin</h3>
+    <span>Volunteer</span>
+  </div>
+</div>
+</div>
+<div class="col-md-6 col-lg-3">
+  <div class="member">
+   <div class="img" style="background-image: url(images/team-6.jpg);"></div>
+   <div class="text text-1">
+    <h3>Cedrick Brown</h3>
+    <span>Volunteer</span>
+  </div>
+</div>
+</div>
+<div class="col-md-6 col-lg-3">
+  <div class="member">
+   <div class="img" style="background-image: url(images/team-7.jpg);"></div>
+   <div class="text text-2">
+    <h3>John Wick</h3>
+    <span>Volunteer</span>
+  </div>
+</div>
+</div>
+<div class="col-md-6 col-lg-3">
+  <div class="member">
+   <div class="img" style="background-image: url(images/team-8.jpg);"></div>
+   <div class="text text-3">
+    <h3>Max Love</h3>
+    <span>Volunteer</span>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+
+</body>
+</html>
