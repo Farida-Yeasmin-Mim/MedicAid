33be9254e3d6086bf4637aa9982cf4e270aa8446
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-26 20:44:52 +0600

    Update covid19_page.php

--- MedicAid+/covid19_page.php
+++ MedicAid+/covid19_page.php
@@ -10,7 +10,7 @@ if (!empty($_GET['id'])){
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Covid19 Bangladesh Update</title>
+    <title>Covid19 | Update</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
