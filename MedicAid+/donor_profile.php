80d1b3ce72dfb81a5da0f3f50464f1962d111bd5
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-26 20:45:03 +0600

    Update donor_profile.php

--- MedicAid+/donor_profile.php
+++ MedicAid+/donor_profile.php
@@ -22,7 +22,7 @@ $dbps= $row["plasma_status"];
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Donor Profile</title>
+    <title>Profile | Donor</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
