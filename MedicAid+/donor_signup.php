a3ec8fe31af343d4ad78e4fd834ee4651a93b341
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-26 22:25:34 +0600

    Update donor_signup.php

--- MedicAid+/donor_signup.php
+++ MedicAid+/donor_signup.php
@@ -77,6 +77,14 @@ header("Location: donor_profile.php?id=$dbid");
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
    <link rel="stylesheet" href="CSS/signup.css">
    <body>
+
+
+
+       <header>
+           <?php include 'header.php';?>
+       </header>
+
+       
      <div class="card bg-dark text-white">
          <img class="card-img" src="Image/signup.jpg" alt="Card image">
        <div class="card-img-overlay">
