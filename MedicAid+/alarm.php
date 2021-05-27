db6f5f6905595d1931d48ee53b5e289b04ad888f
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-27 17:42:54 +0600

    Create alarm.php

---
+++ MedicAid+/alarm.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sound The Alarm</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="CSS/index.css">
+
+  </head>
+  <body>
+
+
+  <header>
+      <?php include 'header.php';?>
+  </header>
+
+  <section class="ftco-hireme bg-info">
+    <div class="container">
+        <div class="col-lg-10 col-md-15 mx-auto text-center">
+          <br>
+          <p class="text-white">SAFETY FIRST! As the COVID-19 pandemic continues,
+            the MedicAid is adapting Sound the Alarm activities to protect the people we serve and our volunteers. Sound the Alarm may look different
+            this year but our lifesaving mission to make families and communities safer from home fire continues. </p>
+            <br>
+        </div>
+    </div>
+  </section>
+
+  <section class="ftco-hireme bg-white">
+    <div class="container">
+        <div class="col-lg-10 col-md-12 mx-auto text-center">
+          <h1 class="text-center font-weight-bold text-danger pt-4 mt-4">Join the MedicAid to Sound the Alarm. Save a Life.</h1>
+        </div>
+    </div>
+  </section>
+
+
+  <div class="wrap-about text-center ">
+    <div class="heading-section pr-md-5 pt-md-5 ">
+    <strong><h1 class="mb-4 text-dark">In a Home Fire, Can Your Family Safely Escape in Just 2 Minutes?</h1></strong>
+  <br>
+  <div class="row justify-content-center">
+  <iframe width="500" height="300" src="https://www.youtube.com/embed/jmEThU6sIjc" allowfullscreen></iframe>
+  </div>
+  </div>
+</div>
+
+
+<br>
+<div class="col-md-20 heading-section ftco-animate p-7 p-lg-5">
+<strong>Q: Are individuals who received a COVID-19 vaccine eligible to give blood, platelets and plasma?</strong>
+<br>
+<strong>A:</strong> In most cases, there is no blood, platelet or plasma donation deferral time after receiving a COVID-19 vaccine. However, knowing the name of the vaccine manufacturer is important in determining donation eligibility.
+
+<br>
+<br>
+<strong>Q: Are individuals who received a COVID-19 vaccine eligible to give COVID-19 convalescent plasma?</strong><br>
+<strong>A:</strong> While the FDA allows people who have received a COVID-19 vaccine to donate dedicated COVID-19 convalescent plasma within six months of their infection of the virus, the Red Cross discontinued dedicated COVID-19 convalescent plasma donations on March 26 due to declining hospital demand and sufficient industry supply. The Red Cross tests all blood donations for COVID-19 antibodies. Plasma from routine blood and platelet donations that test positive for high-levels of antibodies, and meets other requirements, may be used as convalescent plasma to meet potential future needs of COVID-19 patients.
+
+<br>
+<br>
+<strong>Q: I’ve heard claims that the Red Cross refuses to accept convalescent plasma from individuals who have received a COVID-19 vaccine because it wipes out the antibodies. Is this true?</strong><br>
+<strong>A:</strong>  There are claims circulating that incorrectly state that the Red Cross will not accept convalescent plasma donations from those who have received the COVID-19 vaccine because “the vaccine wipes out those antibodies making the convalescent plasma ineffective in treating other COVID-19 patients.” This is not accurate.
+The FDA allows people who have received a COVID-19 vaccine to donate dedicated COVID-19 convalescent plasma within six months of their infection of the virus, based on data that antibodies from natural infection can decline after six months.
+The Red Cross discontinued dedicated COVID-19 convalescent plasma donations on March 26 due to declining hospital demand and sufficient industry supply. The Red Cross tests all blood donations for COVID-19 antibodies. Plasma from routine blood and platelet donations that test positive for high-levels of antibodies, and meets other requirements, may be used as convalescent plasma to meet potential future needs of COVID-19 patients.
+
+<br>
+<br>
+<strong>Q: Does giving blood after receiving a COVID-19 vaccine reduce a donor’s protection from the virus?</strong><br>
+<strong>A:</strong> Donating blood after receiving a COVID-19 vaccine does not reduce a donor’s protection from the virus. Similar to other vaccines such as measles, mumps or influenza, the COVID-19 vaccine is designed to generate an immune response to help protect an individual from illness. A donor’s immune response is not impacted by giving blood.
+
+<br>
+<br>
+<strong>Q: What is the difference between plasma donation and convalescent plasma donation?</strong><br>
+<strong>A:</strong> Plasma donation and COVID-19 convalescent plasma donation are different. Plasma has been collected and transfused since the 1940’s to help patients being treated for a number of medical reasons, including burns, shock, trauma and other injuries and illnesses.
+<br>
+</div>
+
+
+
+<Footer>
+  <?php include 'footer.php';?>
+</Footer>
+
+</body>
+</html>
