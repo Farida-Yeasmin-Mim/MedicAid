1ced96025aa0161c7d3f209305b461f48ce349b8
KaziRamisaRifa <kazi.rifa@northsouth.edu>
2021-05-27 10:56:32 +0600

    Admin Blocking Ambulance Works

---
+++ MedicAid+/report_ambulance.php
@@ -0,0 +1,100 @@
+<?php
+
+  // Create database connection
+  include 'connection.php';
+
+  $dbid =  $_GET['id'];
+
+  if (isset($_POST['search'])) {
+    $dbsearch = strip_tags($_POST['srch']);
+    $sql = "SELECT * FROM ambulance WHERE owner_name LIKE '%$dbsearch%' OR division LIKE '%$dbsearch%'";
+  }
+    else
+    {
+      $sql = "SELECT * FROM ambulance";
+    }
+
+    if (isset($_POST['block'])) {
+      $dbp = $_GET['p'];
+      $dbid =  $_GET['id'];
+      $sql = "DELETE FROM ambulance
+      WHERE ambulance_id = $dbp";
+      mysqli_query($conn, $sql);
+      header("location:report_ambulance.php?id=$dbid");
+    }
+
+$result = mysqli_query($conn, $sql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+<title>Admin | User</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+<link rel="stylesheet" href="CSS/blood_donor_information.css">
+
+
+<!-- Bootstrap Grid System-->
+<link rel="stylesheet" href="CSS/bootstrap-grid.css">
+
+
+<style>
+body {
+background-image: url('Image/blood_donor_information_bg.jpg');
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-size: cover;
+}
+</style>
+</head>
+<body>
+  <header>
+    <?php include 'header2.php';?>
+
+              </header>
+              <br>
+              <button class="btn bg-primary text-white" onclick="window.location ='admin_profile.php?id=<?php echo $dbid ?>'">Back To Profile</button>
+
+                <div class="container-fluid">
+                  <h1 class="text-center text-dark text-capitalize pt-5">Donor Information</h1>
+                  <hr class="w-25 pt-5">
+              <form class="example" method="POST" action="report_ambulance.php?id=<?php echo $dbid ?>">
+  <input name="srch" type="text" placeholder="Search By Name Or Division.." name="search">
+  <button name="search" type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+<div id="content">
+  <?php
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div id='img_div'>";
+        echo "<h6><strong>Name: </strong>".$row['owner_name']." </h6>";
+        echo "<h6><strong>Contact No: </strong>".$row['contact_number']." </h6>";
+        echo "<h6><strong>Ambulance Type: </strong>".$row['type']." </h6>";
+        echo "<h6><strong>Ambulance Number: </strong>".$row['ambulance_number']." </h6>";
+        echo "<h6><strong>Division: </strong>".$row['division']." </h6>";
+        echo "<form method='POST' action='report_ambulance.php?id=$dbid&p=".$row['ambulance_id']."' >";
+        echo "<button type='submit' class='btn btn-danger btn-block' name='block'>Block This User</button>";
+        echo "</form>";
+
+
+      echo "</div>";
+    }
+
+  ?>
+
+
+</div>
+</body>
+</html>
