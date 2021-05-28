937f14188263f727b46cc2e11ff9556ce97f5858
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-28 07:11:58 +0600

    Update user_profile.php

--- MedicAid+/user_profile.php
+++ MedicAid+/user_profile.php
@@ -19,7 +19,7 @@ if ($_GET['id']==NULL){
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Profile | User</title>
+    <title>Profile || User</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
