4b2bef25b6448dca3cc09015f61d5420bf94d52d
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-28 07:12:12 +0600

    Update volunteer_information.php

--- MedicAid+/volunteer_information.php
+++ MedicAid+/volunteer_information.php
@@ -31,7 +31,7 @@ $result = mysqli_query($conn, $sql);
 <html>
 <head>
   <meta charset="utf-8">
-<title>Blood Donor Information</title>
+<title>Volunteer Information</title>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
