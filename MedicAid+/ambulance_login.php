ddd7f8cab95f67ab7b470b8f39f356c84029bed2
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-28 07:10:09 +0600

    Update ambulance_login.php

--- MedicAid+/ambulance_login.php
+++ MedicAid+/ambulance_login.php
@@ -43,7 +43,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Ambulance Login</title>
+    <title>Login || Ambulance </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
