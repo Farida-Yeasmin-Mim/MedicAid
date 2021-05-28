9c237c36d26c02e8f878cc6156c9a8002507b305
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-28 07:10:43 +0600

    Update donor_login.php

--- MedicAid+/donor_login.php
+++ MedicAid+/donor_login.php
@@ -43,7 +43,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Login | Donor</title>
+    <title>Login || Donor</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
