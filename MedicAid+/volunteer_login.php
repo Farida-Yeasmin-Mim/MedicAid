05fde41ef20c05f85d0260df23a24f59b172355e
Yeasmin Mim <faida.yeasmin@northsouth.edu>
2021-05-28 07:12:25 +0600

    Update volunteer_login.php

--- MedicAid+/volunteer_login.php
+++ MedicAid+/volunteer_login.php
@@ -44,7 +44,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Log In | Volunteer</title>
+    <title>Log In || Volunteer</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
